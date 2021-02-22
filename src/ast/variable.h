64fdcd3cb3102ab1aba3a7db5d818972de96a705
MaxwellGengYF <maxwellgeng@outlook.com>
2021-02-22 18:55:21 +0800

    ast header init version

--- src/ast/variable.h
+++ src/ast/variable.h
@@ -3,27 +3,49 @@
 //
 
 #pragma once
-
+#ifndef LC_BACKEND
 #include <ast/type.h>
 #include <core/union.h>
-#include "interface/ivariable.h"
+#endif
 
 namespace luisa::compute {
+class IType;
+class Variable {
+    friend class Function;
 
-class Variable : public IVariable {
+public:
+    enum struct Tag : uint32_t {
+
+        // data
+        LOCAL,
+        SHARED,
+        CONSTANT,
+
+        UNIFORM,
+
+        // resources
+        BUFFER,
+        TEXTURE,
+        // TODO: Bindless Texture
+
+        // builtins
+        THREAD_ID,
+        BLOCK_ID,
+        DISPATCH_ID
+    };
 
 private:
-    const Type *_type;
+    const IType *_type;
     Tag _tag;
     uint32_t _uid;
-
-private:
-    friend class Function;
-    constexpr Variable(const Type *type, Tag tag, uint32_t uid) noexcept
+    constexpr Variable(const IType *type, Tag tag, uint32_t uid) noexcept
         : _type{type}, _tag{tag}, _uid{uid} {}
 
 public:
-    [[nodiscard]] auto type() const noexcept { return _type; }
+#ifndef LC_BACKEND
+    [[nodiscard]] auto type() const noexcept { return static_cast<Type const *>(_type); }
+#endif
+    [[nodiscard]] auto itype() const noexcept { return _type; }
     [[nodiscard]] auto uid() const noexcept { return _uid; }
     [[nodiscard]] auto tag() const noexcept { return _tag; }
 };
