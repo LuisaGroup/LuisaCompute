b8233d4c3f7f29db3fe771c60ceacdae0efcc83b
mike <mango@live.cn>
2021-02-28 13:05:34 +0800

    added basic DSL Var

--- src/tests/test_dsl.cpp
+++ src/tests/test_dsl.cpp
@@ -7,84 +7,10 @@
 
 using namespace luisa;
 using namespace luisa::compute;
-
-namespace internal {
-
-template<typename T>
-struct DeductionGuide;
-
-template<typename T>
-struct Something {};
-
-template<typename T>
-struct DeductionGuideBase {
-    constexpr DeductionGuideBase(T s) noexcept { LUISA_INFO("From scalar: {}", Type::of(s)->description()); }
-    constexpr DeductionGuideBase(Something<T>) noexcept { LUISA_INFO("From something..."); }
-    constexpr DeductionGuideBase(DeductionGuideBase &&) noexcept { LUISA_INFO("From another guide..."); }
-    constexpr DeductionGuideBase(const DeductionGuideBase &) noexcept { LUISA_INFO("From another guide..."); }
-    constexpr DeductionGuideBase(DeductionGuide<T> &&) noexcept { LUISA_INFO("From another guide..."); }
-    constexpr DeductionGuideBase(const DeductionGuide<T> &) noexcept { LUISA_INFO("From another guide..."); }
-
-    void operator=(const DeductionGuideBase &) const noexcept { LUISA_INFO("operator=(const &)"); }
-    void operator=(DeductionGuideBase &&) const noexcept { LUISA_INFO("operator=(&&)"); }
-    void operator+=(const DeductionGuideBase &) const noexcept { LUISA_INFO("operator+=(const &)"); }
-    void operator+=(DeductionGuideBase &&) const noexcept { LUISA_INFO("operator+=(&&)"); }
-
-    template<typename U>
-    void operator+(DeductionGuide<U>) const noexcept { LUISA_INFO("operator+(DeductionGuide)"); }
-
-    template<typename U>
-    void operator+(const Something<U> &) const noexcept { LUISA_INFO("operator+(const Something &)"); }
-
-    //    template<typename U>
-    //    void operator+(Something<U> &&) const noexcept { LUISA_INFO("operator+(Something &&)"); }
-
-    template<concepts::Native U>
-    void operator+(U &&) const noexcept { LUISA_INFO("operator+(U)"); }
-
-    template<concepts::Native U>
-    friend void operator+(U &&, DeductionGuideBase) noexcept { LUISA_INFO("friend operator+(U)"); }
-};
-
-template<typename T>
-struct DeductionGuide : public DeductionGuideBase<T> {
-    using DeductionGuideBase<T>::DeductionGuideBase;
-    //    using DeductionGuideBase<T>::operator=;
-};
-
-template<typename T>
-DeductionGuide(const Something<T> &) -> DeductionGuide<T>;
-
-template<typename T>
-DeductionGuide(Something<T> &&) -> DeductionGuide<T>;
-
-template<typename T>
-DeductionGuide(DeductionGuide<T>) -> DeductionGuide<T>;
-
-template<typename T>
-DeductionGuide(T &&) -> DeductionGuide<std::remove_cvref_t<T>>;
-
-}// namespace internal
+using namespace luisa::compute::dsl;
 
 int main() {
-
-    internal::DeductionGuide g1 = 1.5f;
-    internal::DeductionGuide g2{internal::Something<int>{}};
-    internal::DeductionGuide g3{g1};
-    internal::DeductionGuide g4{internal::DeductionGuide{float2{1.0f}}};
-
-    internal::Something<float> f;
-    g3 = f;
-    g3 = 1.2f;
-    g3 + f;
-    g3 + internal::DeductionGuide{1.5f};
-    g3 + internal::Something<float>{};
-    g3 + 1.0f;
-    1.0f + g3;
-    g3 += 1;
-
-    using namespace luisa::compute::dsl;
-
+    
     FunctionBuilder{Function::Tag::KERNEL}.define([] {
         
         Var v_int = 10;
