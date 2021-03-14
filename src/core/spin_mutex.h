eed5c0b80db2584cb74a5a0758d738a53ebb91d6
mike <mango@live.cn>
2021-03-14 20:27:39 +0800

    support arm64

--- src/core/spin_mutex.h
+++ src/core/spin_mutex.h
@@ -16,9 +16,11 @@ private:
 public:
     void lock() noexcept {
         while (_flag.test_and_set(std::memory_order::acquire)) {// acquire lock
-            while (_flag.test(std::memory_order::relaxed)) { // test lock
+            while (_flag.test(std::memory_order::relaxed)) {    // test lock
 #if defined(__x86_64__) || defined(_M_X64)
                 _mm_pause();
+#elif defined(__aarch64__) || defined(_M_ARM64)
+                __asm__ __volatile__("isb\n");
 #endif
             }
         }
