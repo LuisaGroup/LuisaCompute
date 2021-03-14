bad93dd002da41bab3c31888d02dea22985de917
mike <mango@live.cn>
2021-03-14 19:11:38 +0800

    using/Users/mike/ClionProjects/LuisaCompute/build/bin/test_dsl

---
+++ src/core/spin_mutex.h
@@ -0,0 +1,28 @@
+//
+// Created by Mike Smith on 2021/3/14.
+//
+
+#pragma once
+
+#include <atomic>
+#include <thread>
+
+namespace luisa {
+
+class spin_mutex {
+
+private:
+    std::atomic_flag _flag;
+
+public:
+    void lock() noexcept {
+        while (_flag.test_and_set(std::memory_order_acquire))// acquire lock
+            while (_flag.test(std::memory_order_relaxed)) {} // test lock
+    }
+
+    void unlock() noexcept {
+        _flag.clear(std::memory_order_release);// release lock
+    }
+};
+
+}// namespace luisa
