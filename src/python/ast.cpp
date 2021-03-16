5a03d493e05207ee352ebd97c79683ed3da4502f
mike <mango@live.cn>
2021-03-16 14:23:22 +0800

    added pybind

---
+++ src/python/ast.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Mike Smith on 2021/3/16.
+//
+
+#include <pybind11/pybind11.h>
+#include <ast/function_builder.h>
+
+namespace luisa::compute::python {
+
+
+
+}
+
+PYBIND11_MODULE(ast, m) {
+
+}
