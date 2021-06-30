07c0a2c96e2afc9889f97ba7c02cbacaf164784b
mike <mango@live.cn>
2021-06-30 23:53:30 +0800

    lazy alloc ring buffer

--- src/backends/metal/metal_ring_buffer.h
+++ src/backends/metal/metal_ring_buffer.h
@@ -12,19 +12,17 @@ namespace luisa::compute::metal {
 class MetalRingBuffer {
 
 private:
-    id<MTLBuffer> _buffer;
+    id<MTLBuffer> _buffer{nullptr};
+    id<MTLDevice> _device;
     size_t _size;
     size_t _free_begin;
     size_t _free_end;
+    bool _optimize_write;
     spin_mutex _mutex;
 
 public:
     MetalRingBuffer(id<MTLDevice> device, size_t size, bool optimize_write) noexcept
-        : _size{size}, _free_begin{0u}, _free_end{size} {
-        auto buffer_options = MTLResourceStorageModeShared | MTLResourceHazardTrackingModeUntracked;
-        if (optimize_write) { buffer_options |= MTLResourceCPUCacheModeWriteCombined; }
-        _buffer = [device newBufferWithLength:size options:buffer_options];
-    }
+        : _device{device}, _size{size}, _free_begin{0u}, _free_end{size}, _optimize_write{optimize_write} {}
     ~MetalRingBuffer() noexcept { _buffer = nullptr; }
     [[nodiscard]] MetalBufferView allocate(size_t size) noexcept;
     void recycle(const MetalBufferView &view) noexcept;
--- src/backends/metal/metal_ring_buffer.mm
+++ src/backends/metal/metal_ring_buffer.mm
@@ -11,6 +11,11 @@ namespace luisa::compute::metal {
 
 MetalBufferView MetalRingBuffer::allocate(size_t size) noexcept {
     std::scoped_lock lock{_mutex};
+    if (_buffer == nullptr) {
+        auto buffer_options = MTLResourceStorageModeShared | MTLResourceHazardTrackingModeUntracked;
+        if (_optimize_write) { buffer_options |= MTLResourceCPUCacheModeWriteCombined; }
+        _buffer = [_device newBufferWithLength:size options:buffer_options];
+    }
     auto offset = _free_begin % _size;
     auto free_next = _free_begin + size;
     if (offset + size > _size) {
