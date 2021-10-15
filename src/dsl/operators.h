4ece004266d6ecc14b57da9888c17e068a87c287
mike <mango@live.cn>
2021-10-15 15:49:25 +0800

    disabling cuda on ci

--- src/dsl/operators.h
+++ src/dsl/operators.h
@@ -59,14 +59,15 @@ LUISA_MAKE_GLOBAL_DSL_BINARY_OP(>, GREATER)
 LUISA_MAKE_GLOBAL_DSL_BINARY_OP(>=, GREATER_EQUAL)
 #undef LUISA_MAKE_GLOBAL_DSL_BINARY_OP
 
-#define LUISA_MAKE_GLOBAL_DSL_ASSIGN_OP(op, op_tag_name)                                              \
-    template<typename T, typename U>                                                                  \
-        requires requires { std::declval<T &>() op std::declval<luisa::compute::expr_value_t<U>>(); } \
-    void operator op(luisa::compute::Var<T> &lhs, U &&rhs) noexcept {                                 \
-        luisa::compute::detail::FunctionBuilder::current()->assign(                                   \
-            luisa::compute::AssignOp::op_tag_name,                                                    \
-            lhs.expression(),                                                                         \
-            luisa::compute::detail::extract_expression(std::forward<U>(rhs)));                        \
+#define LUISA_MAKE_GLOBAL_DSL_ASSIGN_OP(op, op_tag_name)                       \
+    template<typename T, typename U>                                           \
+        requires requires { std::declval<T &>() op                             \
+                            std::declval<luisa::compute::expr_value_t<U>>(); } \
+    void operator op(luisa::compute::Var<T> &lhs, U &&rhs) noexcept {          \
+        luisa::compute::detail::FunctionBuilder::current()->assign(            \
+            luisa::compute::AssignOp::op_tag_name,                             \
+            lhs.expression(),                                                  \
+            luisa::compute::detail::extract_expression(std::forward<U>(rhs))); \
     }
 LUISA_MAKE_GLOBAL_DSL_ASSIGN_OP(+=, ADD_ASSIGN)
 LUISA_MAKE_GLOBAL_DSL_ASSIGN_OP(-=, SUB_ASSIGN)
