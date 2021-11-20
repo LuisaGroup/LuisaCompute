caee60499ba7c8ec9a8f06cc61672ee18ff97fd2
mike <mango@live.cn>
2021-11-20 21:03:19 +0800

    no good

--- src/tests/network/test_network_client.cpp
+++ src/tests/network/test_network_client.cpp
@@ -7,6 +7,7 @@
 #include <stb/stb_image.h>
 #include <opencv2/opencv.hpp>
 
+#include <core/clock.h>
 #include <network/render_config.h>
 #include <network/render_client.h>
 
@@ -17,10 +18,16 @@ int main() {
     auto client = RenderClient::create("166.111.69.34", 23456u);
     client->set_display_handler([](const RenderConfig &config, size_t frame_count, std::span<const std::byte> data) noexcept {
               LUISA_INFO("Received frame (spp = {}, size = {}).", frame_count, data.size_bytes());
-              cv::_InputArray array{
-                  reinterpret_cast<const uint8_t *>(data.data()),
-                  static_cast<int>(data.size())};
-              auto image = cv::imdecode(array, cv::IMREAD_COLOR);
+              int width, height, channels;
+              Clock clock;
+              std::unique_ptr<uint8_t, void (*)(void *)> pixels{
+                  stbi_load_from_memory(
+                      reinterpret_cast<const uint8_t *>(data.data()), data.size_bytes(),
+                      &width, &height, &channels, 3),
+                  stbi_image_free};
+              LUISA_INFO("Decode: {} ms.", clock.toc());
+              cv::Mat image{height, width, CV_8UC3, pixels.get()};
+              cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
               cv::imshow("Display", image);
               cv::waitKey(1);
           })
