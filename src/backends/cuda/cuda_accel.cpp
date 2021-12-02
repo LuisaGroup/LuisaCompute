5d719b8859d6e0e447db603273d17351ef75bda9
郑少锟 <Mango@Live.cn>
2021-12-02 17:35:43 +0800

    working on cuda accel

--- src/backends/cuda/cuda_accel.cpp
+++ src/backends/cuda/cuda_accel.cpp
@@ -2,10 +2,23 @@
 // Created by Mike on 2021/12/2.
 //
 
+#include <backends/cuda/cuda_error.h>
 #include <backends/cuda/cuda_accel.h>
 
 namespace luisa::compute::cuda {
 
+CUDAAccel::CUDAAccel(AccelBuildHint hint) noexcept
+    : _build_hint{hint} {
+    LUISA_CHECK_CUDA(cuEventCreate(
+        &_update_event,
+        CU_EVENT_DISABLE_TIMING));
+}
 
+CUDAAccel::~CUDAAccel() noexcept {
+    LUISA_CHECK_CUDA(cuMemFree(_instance_buffer));
+    LUISA_CHECK_CUDA(cuMemFree(_update_buffer));
+    LUISA_CHECK_CUDA(cuMemFree(_bvh_buffer));
+    LUISA_CHECK_CUDA(cuEventDestroy(_update_event));
+}
 
 }
--- src/backends/cuda/cuda_accel.h
+++ src/backends/cuda/cuda_accel.h
@@ -4,9 +4,39 @@
 
 #pragma once
 
+#include <vector>
+
+#include <cuda.h>
+#include <optix.h>
+
+#include <rtx/accel.h>
+#include <core/dirty_range.h>
+
 namespace luisa::compute::cuda {
 
+class CUDAMesh;
 
+class CUDAAccel {
 
+private:
+    OptixTraversableHandle _handle{};
+    std::vector<CUDAMesh *> _instance_meshes;
+    std::vector<float4x4> _instance_transforms;
+    std::vector<OptixInstance> _instances;
+    CUdeviceptr _instance_buffer{};
+    CUdeviceptr _bvh_buffer{};
+    size_t _bvh_buffer_size{};
+    CUdeviceptr _update_buffer{};
+    size_t _update_buffer_size{};
+    CUevent _update_event{};
+    DirtyRange _dirty_range{};
+    AccelBuildHint _build_hint;
+
+public:
+    CUDAAccel(AccelBuildHint hint) noexcept;
+    ~CUDAAccel() noexcept;
+    void add_instance(CUDAMesh *mesh, float4x4 transform) noexcept;
+    void set_transform(size_t index, float4x4 transform) noexcept;
+};
 
 }
