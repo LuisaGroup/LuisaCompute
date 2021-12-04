61818178fdf19fa88a4814735692f977e838f0bc
郑少锟 <Mango@Live.cn>
2021-12-04 15:10:36 +0800

    prepare for optimization

--- src/tests/test_rtx.cpp
+++ src/tests/test_rtx.cpp
@@ -85,19 +85,7 @@ int main(int argc, char *argv[]) {
             constexpr auto red = float3(1.0f, 0.0f, 0.0f);
             constexpr auto green = float3(0.0f, 1.0f, 0.0f);
             constexpr auto blue = float3(0.0f, 0.0f, 1.0f);
-            $if(hit.inst == 0u) {
-                color = red;
-            }
-            $elif(hit.inst == 1u) {
-                color = green;
-            }
-            $elif(hit.inst == 2u) {
-                color = blue;
-            }
-            $else {
-                color = make_float3();
-            };
-            //            color = interpolate(hit, red, green, blue);
+            color = interpolate(hit, red, green, blue);
         };
         auto old = image.read(coord).xyz();
         auto t = 1.0f / (frame_index + 1.0f);
