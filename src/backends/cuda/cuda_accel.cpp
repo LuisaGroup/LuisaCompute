72fd56c100661ead0a7897947fa1720ccae194a1
郑少锟 <Mango@Live.cn>
2021-12-04 16:54:02 +0800

    fix link with optix

--- src/backends/cuda/cuda_accel.cpp
+++ src/backends/cuda/cuda_accel.cpp
@@ -2,6 +2,8 @@
 // Created by Mike on 2021/12/2.
 //
 
+#include <optix_stubs.h>
+
 #include <backends/cuda/cuda_error.h>
 #include <backends/cuda/cuda_stream.h>
 #include <backends/cuda/cuda_device.h>
