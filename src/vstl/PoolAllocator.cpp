b85532f1edbf21033874ea10312e1fd7b6fc742a
郑少锟 <Mango@Live.cn>
2021-12-21 16:09:50 +0800

    is vstl evil?

--- src/vstl/PoolAllocator.cpp
+++ src/vstl/PoolAllocator.cpp
@@ -2,26 +2,26 @@
 #include <vstl/PoolAllocator.h>
 namespace vstd {
 PoolAllocator::AllocateHandle PoolAllocator::Allocate() {
-	if (allocatedData.empty()) {
-		auto ptr = data.emplace_back(visitor->Allocate(stride * elementCount));
-		allocatedData.push_back_func(
-			[&](size_t i) -> std::pair<void*, size_t> {
-				return {ptr, i};
-			},
-			elementCount);
-	}
-	auto result = allocatedData.erase_last();
-	return {
-		result.first,
-		result.second};
+    if (allocatedData.empty()) {
+        auto ptr = data.emplace_back(visitor->Allocate(stride * elementCount));
+        allocatedData.push_back_func(
+            [&](size_t i) -> std::pair<void *, size_t> {
+                return {ptr, i};
+            },
+            elementCount);
+    }
+    auto result = allocatedData.erase_last();
+    return {
+        result.first,
+        result.second};
 }
-void PoolAllocator::DeAllocate(AllocateHandle const& handle) {
-	allocatedData.emplace_back(handle.resource, handle.offset);
+void PoolAllocator::DeAllocate(AllocateHandle const &handle) {
+    allocatedData.emplace_back(handle.resource, handle.offset);
 }
 PoolAllocator::~PoolAllocator() {
-	for (auto&& i : data) {
-		visitor->DeAllocate(i);
-	}
+    for (auto &&i : data) {
+        visitor->DeAllocate(i);
+    }
 }
 
 }// namespace vstd
