9f1fb9842773291093df9bbb07274d437d33be64
Mike Smith <mango@live.cn>
2021-12-25 08:09:44 +0800

    using eastl::lru_cache

--- src/core/lru_cache.h
+++ src/core/lru_cache.h
@@ -11,18 +11,16 @@
 
 namespace luisa {
 
-template<typename Key, typename Value, typename Hash = luisa::Hash64>
+// TODO: support allocator & comparator
+template<typename Key, typename Value>
 class LRUCache {
 
 private:
-    luisa::unordered_map<Key, uint64_t, Hash> _key_to_timepoint;
-    luisa::map<uint64_t, std::pair<Key, Value>> _timepoint_to_key_and_value;
-    size_t _capacity;
-    uint64_t _current_timepoint{0u};
+    luisa::lru_cache<Key, Value> _cache;
     std::mutex _mutex;
 
 public:
-    explicit LRUCache(size_t cap) noexcept : _capacity{cap} {}
+    explicit LRUCache(size_t cap) noexcept : _cache{cap} {}
     LRUCache(LRUCache &&) noexcept = delete;
     LRUCache(const LRUCache &) noexcept = delete;
     LRUCache &operator=(LRUCache &&) noexcept = delete;
@@ -34,52 +32,14 @@ public:
 
     [[nodiscard]] auto fetch(const Key &key) noexcept -> luisa::optional<Value> {
         std::scoped_lock lock{_mutex};
-        auto timepoint_iter = _key_to_timepoint.find(key);
-        // not in cache
-        if (timepoint_iter == _key_to_timepoint.end()) {
-            return luisa::nullopt;
-        }
-        // in cache, update timepoint
-        auto old_timepoint = timepoint_iter->second;
-        auto new_timepoint = ++_current_timepoint;
-        timepoint_iter->second = new_timepoint;
-        auto iter = _timepoint_to_key_and_value.find(old_timepoint);
-        auto value = std::move(iter->second.second);
-        auto copy = value;
-        _timepoint_to_key_and_value.erase(iter);
-        _timepoint_to_key_and_value.emplace(
-            new_timepoint,
-            std::make_pair(key, std::move(value)));
-        return copy;
+        auto value = _cache.at(key);
+        _cache.touch(key);
+        return value;
     }
 
     void update(const Key &key, Value value) noexcept {
         std::scoped_lock lock{_mutex};
-        // already in the cache, just update
-        auto item = std::make_pair(key, std::move(value));
-        if (auto timepoint_iter = _key_to_timepoint.find(key);
-            timepoint_iter != _key_to_timepoint.end()) {
-            // update timepoint
-            auto old_timepoint = timepoint_iter->second;
-            auto new_timepoint = ++_current_timepoint;
-            timepoint_iter->second = new_timepoint;
-            // update the cache item
-            _timepoint_to_key_and_value.erase(old_timepoint);
-            _timepoint_to_key_and_value.emplace(new_timepoint, item);
-        } else {
-            // remove the least recently used item if cache exceeds the limit
-            if (_key_to_timepoint.size() >= _capacity) {
-                // note: map is sorted, so begin() is the least recently used
-                auto lru_iter = _timepoint_to_key_and_value.begin();
-                auto lru_key = lru_iter->second.first;
-                _timepoint_to_key_and_value.erase(lru_iter);
-                _key_to_timepoint.erase(lru_key);
-            }
-            // emplace the new item
-            auto timepoint = ++_current_timepoint;
-            _key_to_timepoint.emplace(key, timepoint);
-            _timepoint_to_key_and_value.emplace(timepoint, item);
-        }
+        _cache.emplace(key, std::move(value));
     }
 };
 
