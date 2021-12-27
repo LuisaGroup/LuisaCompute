d397f112a42b79216214fed51ef97427709b261c
Mike Smith <mango@live.cn>
2021-12-28 02:05:34 +0800

    remove world to object transform

--- src/rtx/hit.h
+++ src/rtx/hit.h
@@ -12,7 +12,6 @@ struct alignas(16) Hit {
     uint inst{0u};
     uint prim{0u};
     float2 uv;
-    float4x4 world_to_object;
     float4x4 object_to_world;
 };
 
@@ -23,7 +22,7 @@ struct alignas(16) Hit {
 
 }// namespace luisa::compute
 
-LUISA_STRUCT(luisa::compute::Hit, inst, prim, uv, world_to_object, object_to_world){
+LUISA_STRUCT(luisa::compute::Hit, inst, prim, uv, object_to_world){
     [[nodiscard]] auto miss() const noexcept {
         return luisa::compute::miss(*this);
     }
