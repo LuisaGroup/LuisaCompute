2896f431e1f1f88ecc9559b543a85230df3fac6f
MaxwellGengYF <18963092533@163.com>
2021-12-29 11:02:13 +0800

    vstl 2 eastl, template concept fixed

--- src/vstl/functional.h
+++ src/vstl/functional.h
@@ -6,12 +6,12 @@
 #include <type_traits>
 #include <new>
 #include <vstl/VAllocator.h>
-#include <vector>
 #include <EASTL/functional.h>
 namespace vstd {
 template<typename T>
 using function = eastl::function<T>;
-
+template<typename T>
+using move_only_func = eastl::move_only_function<T>;
 template<typename T>
 decltype(auto) MakeRunnable(T &&functor) {
     return function<FuncType<std::remove_cvref_t<T>>>(functor);
