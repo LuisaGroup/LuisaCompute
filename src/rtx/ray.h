5967b2dc6579c57d5e702833ab905148c17eb721
Mike Smith <mango@live.cn>
2022-01-11 16:37:25 +0800

    refactor struct Ray

--- src/rtx/ray.h
+++ src/rtx/ray.h
@@ -10,23 +10,32 @@
 namespace luisa::compute {
 
 struct alignas(16) Ray {
-    std::array<float, 3> origin;
-    float t_min;
-    std::array<float, 3> direction;
-    float t_max;
+    std::array<float, 3> compressed_origin;
+    float compressed_t_min;
+    std::array<float, 3> compressed_direction;
+    float compressed_t_max;
 };
 
 }
 
-LUISA_STRUCT(luisa::compute::Ray, origin, t_min, direction, t_max) {};
+LUISA_STRUCT(
+    luisa::compute::Ray,
+    compressed_origin,
+    compressed_t_min,
+    compressed_direction,
+    compressed_t_max) {
+
+    [[nodiscard]] auto origin() const noexcept { return luisa::compute::def<luisa::float3>(compressed_origin); }
+    [[nodiscard]] auto direction() const noexcept { return luisa::compute::def<luisa::float3>(compressed_direction); }
+    [[nodiscard]] auto t_min() const noexcept { return compressed_t_min; }
+    [[nodiscard]] auto t_max() const noexcept { return compressed_t_max; }
+    void set_origin(luisa::compute::Expr<luisa::float3> origin) noexcept { compressed_origin = origin; }
+    void set_direction(luisa::compute::Expr<luisa::float3> direction) noexcept { compressed_direction = direction; }
+    void set_t_min(luisa::compute::Expr<float> t_min) noexcept { compressed_t_min = t_min; }
+    void set_t_max(luisa::compute::Expr<float> t_max) noexcept { compressed_t_max = t_max; }
+};
 
 namespace luisa::compute {
-
-[[nodiscard]] Var<float3> origin(Expr<Ray> ray) noexcept;
-[[nodiscard]] Var<float3> direction(Expr<Ray> ray) noexcept;
-
-void set_origin(Var<Ray> &ray, Expr<float3> origin) noexcept;
-void set_direction(Var<Ray> &ray, Expr<float3> direction) noexcept;
 
 [[nodiscard]] Var<Ray> make_ray(
     Expr<float3> origin,
