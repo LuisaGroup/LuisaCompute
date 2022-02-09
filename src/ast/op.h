743a2b30b9b02b4448cef63cab9768b4d0426331
MaxwellGengYF <18963092533@163.com>
2022-02-09 15:07:24 +0800

    builtin function comment

--- src/ast/op.h
+++ src/ast/op.h
@@ -128,25 +128,25 @@ enum struct CallOp : uint32_t {
 
     BUFFER_READ,
     BUFFER_WRITE,
-    TEXTURE_READ,
-    TEXTURE_WRITE,
+    TEXTURE_READ, //(texture, coord: uint_vec)
+    TEXTURE_WRITE,//(texture, coord: uint_vec, value: float4)
 
-    BINDLESS_TEXTURE2D_SAMPLE,
-    BINDLESS_TEXTURE2D_SAMPLE_LEVEL,
-    BINDLESS_TEXTURE2D_SAMPLE_GRAD,
-    BINDLESS_TEXTURE3D_SAMPLE,
-    BINDLESS_TEXTURE3D_SAMPLE_LEVEL,
-    BINDLESS_TEXTURE3D_SAMPLE_GRAD,
-    BINDLESS_TEXTURE2D_READ,
-    BINDLESS_TEXTURE3D_READ,
-    BINDLESS_TEXTURE2D_READ_LEVEL,
-    BINDLESS_TEXTURE3D_READ_LEVEL,
-    BINDLESS_TEXTURE2D_SIZE,
-    BINDLESS_TEXTURE3D_SIZE,
-    BINDLESS_TEXTURE2D_SIZE_LEVEL,
-    BINDLESS_TEXTURE3D_SIZE_LEVEL,
+    BINDLESS_TEXTURE2D_SAMPLE,      //(bindless_array, index: uint, uv: float2): float4
+    BINDLESS_TEXTURE2D_SAMPLE_LEVEL,//(bindless_array, index: uint, uv: float2, level: float): float4
+    BINDLESS_TEXTURE2D_SAMPLE_GRAD, //(bindless_array, index: uint, uv: float2, ddx: float2, ddy: float2): float4
+    BINDLESS_TEXTURE3D_SAMPLE,      //(bindless_array, index: uint, uv: float3): float4
+    BINDLESS_TEXTURE3D_SAMPLE_LEVEL,//(bindless_array, index: uint, uv: float3, level: float): float4
+    BINDLESS_TEXTURE3D_SAMPLE_GRAD, //(bindless_array, index: uint, uv: float3, ddx: float3, ddy: float3): float4
+    BINDLESS_TEXTURE2D_READ,        //(bindless_array, index: uint, coord: uint2): float4
+    BINDLESS_TEXTURE3D_READ,        //(bindless_array, index: uint, coord: uint3): float4
+    BINDLESS_TEXTURE2D_READ_LEVEL,  //(bindless_array, index: uint, coord: uint2, level: uint): float4
+    BINDLESS_TEXTURE3D_READ_LEVEL,  //(bindless_array, index: uint, coord: uint3, level: uint): float4
+    BINDLESS_TEXTURE2D_SIZE,        //(bindless_array, index: uint): uint2
+    BINDLESS_TEXTURE3D_SIZE,        //(bindless_array, index: uint): uint3
+    BINDLESS_TEXTURE2D_SIZE_LEVEL,  //(bindless_array, index: uint, level: uint): uint2
+    BINDLESS_TEXTURE3D_SIZE_LEVEL,  //(bindless_array, index: uint, level: uint): uint3
 
-    BINDLESS_BUFFER_READ,
+    BINDLESS_BUFFER_READ,//(bindless_array, index: uint): expr->type()
 
     MAKE_BOOL2,
     MAKE_BOOL3,
