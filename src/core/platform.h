6282238d6b8ab60b434c0f6c37edb6e01c7fc949
Mike Smith <mango@live.cn>
2022-02-11 01:39:37 +0800

    good

--- src/core/platform.h
+++ src/core/platform.h
@@ -16,16 +16,14 @@
 #define LUISA_FORCE_INLINE __forceinline
 #define LUISA_NEVER_INLINE __declspec(noinline)
 #define LUISA_DLL
-#ifdef LUISA_COMPUTE_CORE_INTERNAL
 #define LUISA_EXPORT_API LUISA_EXTERN_C __declspec(dllexport)
-#else
-#define LUISA_EXPORT_API LUISA_EXTERN_C __declspec(dllimport)
-#endif
+#define LUISA_IMPORT_API LUISA_EXTERN_C __declspec(dllimport)
 #else
 #define LUISA_FORCE_INLINE [[gnu::always_inline, gnu::hot]] inline
 #define LUISA_NEVER_INLINE [[gnu::noinline]]
 #define LUISA_DLL
 #define LUISA_EXPORT_API LUISA_EXTERN_C [[gnu::visibility("default")]]
+#define LUISA_IMPORT_API LUISA_EXTERN_C
 #endif
 
 #if defined(_WINDOWS) || defined(_WIN32) || defined(_WIN64)
