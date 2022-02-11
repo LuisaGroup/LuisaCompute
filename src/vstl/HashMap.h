173cebb712d7fc103d9cee2bc048339b0ea2d203
MaxwellGengYF <18963092533@163.com>
2022-02-11 17:32:59 +0800

    hashmap default value type

--- src/vstl/HashMap.h
+++ src/vstl/HashMap.h
@@ -306,7 +306,7 @@ struct HashValue {
         return h(t);
     }
 };
-template<typename K, typename V, typename Hash = HashValue, typename Compare = compare<K>, VEngine_AllocType allocType = VEngine_AllocType::VEngine>
+template<typename K, typename V = void, typename Hash = HashValue, typename Compare = compare<K>, VEngine_AllocType allocType = VEngine_AllocType::VEngine>
 class HashMap {
 public:
     using KeyType = K;
