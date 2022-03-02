deac3ace0a525e50b1b689327d0c4091f82498fb
Mike Smith <mango@live.cn>
2022-03-02 22:12:25 +0800

    ispc good

--- src/backends/ispc/ispc_shader.cpp
+++ src/backends/ispc/ispc_shader.cpp
@@ -62,7 +62,7 @@ ISPCShader::ISPCShader(const Context &ctx, Function func) noexcept {
             "--addressing=32",
 #ifndef NDEBUG
             "-g",
-            "-D LUISA_DEBUG",
+            "-DLUISA_DEBUG",
 #else
             "--opt=disable-assertions",
 #endif
