4248f33f54e2a0fa4536d45ebfc9b7679580e08a
LastMC <last_mc@126.com>
2022-03-03 15:40:18 +0800

    Update ast doc and part of core doc

--- src/core/dirty_range.h
+++ src/core/dirty_range.h
@@ -9,6 +9,10 @@
 
 namespace luisa::compute {
 
+/**
+ * @brief A class to mark dirty range.
+ * Dirty range = [offset, offset + size)
+ */
 class DirtyRange {
 
 private:
@@ -16,11 +20,42 @@ private:
     size_t _size{};
 
 public:
+    /**
+     * @brief Clear all dirty ranges
+     * 
+     */
     void clear() noexcept;
+    /**
+     * @brief Mark index as dirty
+     * 
+     * @param index
+     */
     void mark(size_t index) noexcept;
+    /**
+     * @brief Merge 2 dirty ranges
+     * 
+     * @param r1 dirty range 1
+     * @param r2 dirty range 2
+     * @return DirtyRange 
+     */
     [[nodiscard]] static DirtyRange merge(DirtyRange r1, DirtyRange r2) noexcept;
+    /**
+     * @brief If dirty range is empty(size == 0)
+     * 
+     * @return true / false
+     */
     [[nodiscard]] auto empty() const noexcept { return _size == 0u; }
+    /**
+     * @brief Return start index of dirty range
+     * 
+     * @return size_t
+     */
     [[nodiscard]] auto offset() const noexcept { return _offset; }
+    /**
+     * @brief Return size of dirty range
+     * 
+     * @return size_t
+     */
     [[nodiscard]] auto size() const noexcept { return _size; }
 };
 
