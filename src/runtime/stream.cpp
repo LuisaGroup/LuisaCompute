a5c91b77a9aae781e7f479c6525ef146f3fd1dc3
MaxwellGengYF <18963092533@163.com>
2022-03-04 15:23:02 +0800

    recycle commands, fix reorder

--- src/runtime/stream.cpp
+++ src/runtime/stream.cpp
@@ -15,17 +15,18 @@ Stream Device::create_stream() noexcept {
 void Stream::_dispatch(CommandList list) noexcept {
     if (auto size = list.size();
         size > 1u && device()->requires_command_reordering()) {
-        reorder_visitor->clear();
-        auto now = std::chrono::high_resolution_clock::now();
         auto commands = list.steal_commands();
         for (auto command : commands) {
             command->accept(*reorder_visitor);
         }
-        auto end = std::chrono::high_resolution_clock::now();
         auto lists = reorder_visitor->command_lists();
-        std::cout << (end - now) << '\n';
-
         device()->dispatch(handle(), lists);
+        reorder_visitor->clear();
+        for (auto command : commands) {
+            command->recycle();
+        }
+
+
     } else {
         device()->dispatch(handle(), list);
     }
