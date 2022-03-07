fe9e462509ca717ca909c317621d5dfcd0ac706f
Mike Smith <mango@live.cn>
2022-03-07 17:45:13 +0800

    add LUISA_ASSERT

--- src/core/logging.h
+++ src/core/logging.h
@@ -75,7 +75,7 @@ void log_level_error() noexcept;
 
 #define LUISA_ASSERT(x, fmt, ...)                \
     do {                                         \
-        if (!(x)) {                              \
+        if (!(x)) [[unlikely]] {                 \
             auto msg = luisa::format(            \
                 fmt __VA_OPT__(, ) __VA_ARGS__); \
             LUISA_ERROR_WITH_LOCATION(           \
