5dac9cb0cdc9d6655e5c1eabacb83ee24d8d08f3
郑少锟 <Mango@Live.cn>
2022-04-06 22:53:02 +0800

    disable cuda multiple stream

--- src/backends/cuda/cuda_stream.cpp
+++ src/backends/cuda/cuda_stream.cpp
@@ -12,13 +12,11 @@ namespace luisa::compute::cuda {
 CUDAStream::CUDAStream() noexcept
     : _upload_pool{64_mb, true} {
     _used_streams.reset();
-    int lo, hi;
-    LUISA_CHECK_CUDA(cuCtxGetStreamPriorityRange(&lo, &hi));
-    LUISA_CHECK_CUDA(cuStreamCreateWithPriority(
-        &_worker_streams.front(), CU_STREAM_NON_BLOCKING, hi));
+    LUISA_CHECK_CUDA(cuStreamCreate(
+        &_worker_streams.front(), CU_STREAM_NON_BLOCKING));
     for (auto i = 1u; i < backed_cuda_stream_count; i++) {
-        LUISA_CHECK_CUDA(cuStreamCreateWithPriority(
-            &_worker_streams[i], CU_STREAM_NON_BLOCKING, hi));
+        LUISA_CHECK_CUDA(cuStreamCreate(
+            &_worker_streams[i], CU_STREAM_NON_BLOCKING));
         LUISA_CHECK_CUDA(cuEventCreate(
             &_worker_events[i], CU_EVENT_DISABLE_TIMING));
     }
