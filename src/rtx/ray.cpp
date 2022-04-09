1b1d21474205bf72285735811a33ee1d42de07af
Mike Smith <mango@live.cn>
2022-04-10 00:11:31 +0800

    refactor ray api

--- src/rtx/ray.cpp
+++ src/rtx/ray.cpp
@@ -15,25 +15,17 @@ Var<Ray> make_ray(Expr<float3> origin, Expr<float3> direction) noexcept {
     return make_ray(origin, direction, 0.0f, std::numeric_limits<float>::max());
 }
 
-Var<Ray> make_ray_robust(
-    Expr<float3> p, Expr<float3> ng,
-    Expr<float3> direction, Expr<float> t_max) noexcept {
-
-    static Callable _make_ray_robust = [](Float3 p, Float3 d, Float3 ng, Float t_max) noexcept {
-        constexpr auto origin = 1.0f / 32.0f;
-        constexpr auto float_scale = 1.0f / 65536.0f;
-        constexpr auto int_scale = 256.0f;
-        auto n = faceforward(ng, -d, ng);
-        auto of_i = make_int3(int_scale * n);
-        auto p_i = as<float3>(as<int3>(p) + ite(p < 0.0f, -of_i, of_i));
-        auto ro = ite(abs(p) < origin, p + float_scale * n, p_i);
-        return make_ray(ro, d, 0.f, t_max);
-    };
-    return _make_ray_robust(p, direction, ng, t_max);
+Float3 offset_ray_origin(Expr<float3> p, Expr<float3> n) noexcept {
+    constexpr auto origin = 1.0f / 32.0f;
+    constexpr auto float_scale = 1.0f / 65536.0f;
+    constexpr auto int_scale = 256.0f;
+    auto of_i = make_int3(int_scale * n);
+    auto p_i = as<float3>(as<int3>(p) + ite(p < 0.0f, -of_i, of_i));
+    return ite(abs(p) < origin, p + float_scale * n, p_i);
 }
 
-Var<Ray> make_ray_robust(Expr<float3> p, Expr<float3> ng, Expr<float3> direction) noexcept {
-    return make_ray_robust(p, ng, direction, std::numeric_limits<float>::max());
+Float3 compute::offset_ray_origin(Expr<float3> p, Expr<float3> n, Expr<float3> w) noexcept {
+    return offset_ray_origin(p, faceforward(n, -w, n));
 }
 
 }// namespace luisa::compute
