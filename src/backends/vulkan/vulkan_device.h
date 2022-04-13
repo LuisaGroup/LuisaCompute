efc13c8f7b07e0d33aef0ba0ccff41824ca5f7b1
Mike Smith <mango@live.cn>
2022-04-14 00:51:25 +0800

    add vulkan dependencies

---
+++ src/backends/vulkan/vulkan_device.h
@@ -0,0 +1,11 @@
+//
+// Created by Mike Smith on 2022/4/14.
+//
+
+#ifndef LUISACOMPUTE_VULKAN_DEVICE_H
+#define LUISACOMPUTE_VULKAN_DEVICE_H
+
+class vulkan_device {
+};
+
+#endif//LUISACOMPUTE_VULKAN_DEVICE_H
