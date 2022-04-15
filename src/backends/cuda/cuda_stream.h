f2afb371b597a07f7045d6cb1a7ae55e16c323bc
Mike Smith <mango@live.cn>
2022-04-16 01:03:48 +0800

    fix

--- src/backends/cuda/cuda_stream.h
+++ src/backends/cuda/cuda_stream.h
@@ -24,7 +24,7 @@ class CUDACallbackContext;
 class CUDAStream {
 
 public:
-    static constexpr auto backed_cuda_stream_count = 3u;
+    static constexpr auto backed_cuda_stream_count = 1u;
 
 private:
     CUDAHostBufferPool _upload_pool;
