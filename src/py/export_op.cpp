9972ca5b5e14e494b3535b4bb1e201004f0d7315
LastMC <last_mc@126.com>
2022-04-21 13:29:32 +0800

    Modify python

---
+++ src/py/export_op.cpp
@@ -0,0 +1,173 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+#include <luisa-compute.h>
+
+namespace py = pybind11;
+using namespace luisa::compute;
+
+void export_op(py::module &m) {
+
+    py::enum_<CastOp>(m, "CastOp")
+        .value("STATIC", CastOp::STATIC)
+        .value("BITWISE", CastOp::BITWISE);
+
+    py::enum_<UnaryOp>(m, "UnaryOp")
+        .value("PLUS", UnaryOp::PLUS)
+        .value("MINUS", UnaryOp::MINUS)
+        .value("NOT", UnaryOp::NOT)
+        .value("BIT_NOT", UnaryOp::BIT_NOT);
+
+    py::enum_<BinaryOp>(m, "BinaryOp")
+        // arithmetic
+        .value("ADD", BinaryOp::ADD)
+        .value("SUB", BinaryOp::SUB)
+        .value("MUL", BinaryOp::MUL)
+        .value("DIV", BinaryOp::DIV)
+        .value("MOD", BinaryOp::MOD)
+        .value("BIT_AND", BinaryOp::BIT_AND)
+        .value("BIT_OR", BinaryOp::BIT_OR)
+        .value("BIT_XOR", BinaryOp::BIT_XOR)
+        .value("SHL", BinaryOp::SHL)
+        .value("SHR", BinaryOp::SHR)
+        .value("AND", BinaryOp::AND)
+        .value("OR", BinaryOp::OR)
+        // relational
+        .value("LESS", BinaryOp::LESS)
+        .value("GREATER", BinaryOp::GREATER)
+        .value("LESS_EQUAL", BinaryOp::LESS_EQUAL)
+        .value("GREATER_EQUAL", BinaryOp::GREATER_EQUAL)
+        .value("EQUAL", BinaryOp::EQUAL)
+        .value("NOT_EQUAL", BinaryOp::NOT_EQUAL);
+
+
+	py::enum_<CallOp>(m, "CallOp")
+
+    	.value("CUSTOM", CallOp::CUSTOM)
+
+    	.value("ALL", CallOp::ALL)
+    	.value("ANY", CallOp::ANY)
+
+    	.value("SELECT", CallOp::SELECT)
+    	.value("CLAMP", CallOp::CLAMP)
+    	.value("LERP", CallOp::LERP)
+    	.value("STEP", CallOp::STEP)
+
+    	.value("ABS", CallOp::ABS)
+    	.value("MIN", CallOp::MIN)
+    	.value("MAX", CallOp::MAX)
+
+    	.value("CLZ", CallOp::CLZ)
+    	.value("CTZ", CallOp::CTZ)
+    	.value("POPCOUNT", CallOp::POPCOUNT)
+    	.value("REVERSE", CallOp::REVERSE)
+
+    	.value("ISINF", CallOp::ISINF)
+    	.value("ISNAN", CallOp::ISNAN)
+
+    	.value("ACOS", CallOp::ACOS)
+    	.value("ACOSH", CallOp::ACOSH)
+    	.value("ASIN", CallOp::ASIN)
+    	.value("ASINH", CallOp::ASINH)
+    	.value("ATAN", CallOp::ATAN)
+    	.value("ATAN2", CallOp::ATAN2)
+    	.value("ATANH", CallOp::ATANH)
+
+    	.value("COS", CallOp::COS)
+    	.value("COSH", CallOp::COSH)
+    	.value("SIN", CallOp::SIN)
+    	.value("SINH", CallOp::SINH)
+    	.value("TAN", CallOp::TAN)
+    	.value("TANH", CallOp::TANH)
+
+    	.value("EXP", CallOp::EXP)
+    	.value("EXP2", CallOp::EXP2)
+    	.value("EXP10", CallOp::EXP10)
+    	.value("LOG", CallOp::LOG)
+    	.value("LOG2", CallOp::LOG2)
+    	.value("LOG10", CallOp::LOG10)
+    	.value("POW", CallOp::POW)
+
+    	.value("SQRT", CallOp::SQRT)
+    	.value("RSQRT", CallOp::RSQRT)
+
+    	.value("CEIL", CallOp::CEIL)
+    	.value("FLOOR", CallOp::FLOOR)
+    	.value("FRACT", CallOp::FRACT)
+    	.value("TRUNC", CallOp::TRUNC)
+    	.value("ROUND", CallOp::ROUND)
+
+    	.value("FMA", CallOp::FMA)
+    	.value("COPYSIGN", CallOp::COPYSIGN)
+
+    	.value("CROSS", CallOp::CROSS)
+    	.value("DOT", CallOp::DOT)
+    	.value("LENGTH", CallOp::LENGTH)
+    	.value("LENGTH_SQUARED", CallOp::LENGTH_SQUARED)
+    	.value("NORMALIZE", CallOp::NORMALIZE)
+    	.value("FACEFORWARD", CallOp::FACEFORWARD)
+
+    	.value("DETERMINANT", CallOp::DETERMINANT)
+    	.value("TRANSPOSE", CallOp::TRANSPOSE)
+    	.value("INVERSE", CallOp::INVERSE)
+
+    	.value("SYNCHRONIZE_BLOCK", CallOp::SYNCHRONIZE_BLOCK)
+
+    	.value("ATOMIC_EXCHANGE", CallOp::ATOMIC_EXCHANGE)        /// [(atomic_ref, desired) -> old]: stores desired, returns old.
+    	.value("ATOMIC_COMPARE_EXCHANGE", CallOp::ATOMIC_COMPARE_EXCHANGE)/// [(atomic_ref, expected, desired) -> old]: stores (old == expected ? desired : old), returns old.
+    	.value("ATOMIC_FETCH_ADD", CallOp::ATOMIC_FETCH_ADD)       /// [(atomic_ref, val) -> old]: stores (old + val), returns old.
+    	.value("ATOMIC_FETCH_SUB", CallOp::ATOMIC_FETCH_SUB)       /// [(atomic_ref, val) -> old]: stores (old - val), returns old.
+    	.value("ATOMIC_FETCH_AND", CallOp::ATOMIC_FETCH_AND)       /// [(atomic_ref, val) -> old]: stores (old & val), returns old.
+    	.value("ATOMIC_FETCH_OR", CallOp::ATOMIC_FETCH_OR)        /// [(atomic_ref, val) -> old]: stores (old | val), returns old.
+    	.value("ATOMIC_FETCH_XOR", CallOp::ATOMIC_FETCH_XOR)       /// [(atomic_ref, val) -> old]: stores (old ^ val), returns old.
+    	.value("ATOMIC_FETCH_MIN", CallOp::ATOMIC_FETCH_MIN)       /// [(atomic_ref, val) -> old]: stores min(old, val), returns old.
+    	.value("ATOMIC_FETCH_MAX", CallOp::ATOMIC_FETCH_MAX)       /// [(atomic_ref, val) -> old]: stores max(old, val), returns old.
+
+    	.value("BUFFER_READ", CallOp::BUFFER_READ)  /// [(buffer, index) -> value]: reads the index-th element in buffer
+    	.value("BUFFER_WRITE", CallOp::BUFFER_WRITE) /// [(buffer, index, value) -> void]: writes value into the index-th element of buffer
+    	.value("TEXTURE_READ", CallOp::TEXTURE_READ) /// [(texture, coord) -> value]
+    	.value("TEXTURE_WRITE", CallOp::TEXTURE_WRITE)/// [(texture, coord, value) -> void]
+
+    	.value("BINDLESS_TEXTURE2D_SAMPLE", CallOp::BINDLESS_TEXTURE2D_SAMPLE)      //(bindless_array, index: uint, uv: float2): float4
+    	.value("BINDLESS_TEXTURE2D_SAMPLE_LEVEL", CallOp::BINDLESS_TEXTURE2D_SAMPLE_LEVEL)//(bindless_array, index: uint, uv: float2, level: float): float4
+    	.value("BINDLESS_TEXTURE2D_SAMPLE_GRAD", CallOp::BINDLESS_TEXTURE2D_SAMPLE_GRAD) //(bindless_array, index: uint, uv: float2, ddx: float2, ddy: float2): float4
+    	.value("BINDLESS_TEXTURE3D_SAMPLE", CallOp::BINDLESS_TEXTURE3D_SAMPLE)      //(bindless_array, index: uint, uv: float3): float4
+    	.value("BINDLESS_TEXTURE3D_SAMPLE_LEVEL", CallOp::BINDLESS_TEXTURE3D_SAMPLE_LEVEL)//(bindless_array, index: uint, uv: float3, level: float): float4
+    	.value("BINDLESS_TEXTURE3D_SAMPLE_GRAD", CallOp::BINDLESS_TEXTURE3D_SAMPLE_GRAD) //(bindless_array, index: uint, uv: float3, ddx: float3, ddy: float3): float4
+    	.value("BINDLESS_TEXTURE2D_READ", CallOp::BINDLESS_TEXTURE2D_READ)        //(bindless_array, index: uint, coord: uint2): float4
+    	.value("BINDLESS_TEXTURE3D_READ", CallOp::BINDLESS_TEXTURE3D_READ)        //(bindless_array, index: uint, coord: uint3): float4
+    	.value("BINDLESS_TEXTURE2D_READ_LEVEL", CallOp::BINDLESS_TEXTURE2D_READ_LEVEL)  //(bindless_array, index: uint, coord: uint2, level: uint): float4
+    	.value("BINDLESS_TEXTURE3D_READ_LEVEL", CallOp::BINDLESS_TEXTURE3D_READ_LEVEL)  //(bindless_array, index: uint, coord: uint3, level: uint): float4
+    	.value("BINDLESS_TEXTURE2D_SIZE", CallOp::BINDLESS_TEXTURE2D_SIZE)        //(bindless_array, index: uint): uint2
+    	.value("BINDLESS_TEXTURE3D_SIZE", CallOp::BINDLESS_TEXTURE3D_SIZE)        //(bindless_array, index: uint): uint3
+    	.value("BINDLESS_TEXTURE2D_SIZE_LEVEL", CallOp::BINDLESS_TEXTURE2D_SIZE_LEVEL)  //(bindless_array, index: uint, level: uint): uint2
+    	.value("BINDLESS_TEXTURE3D_SIZE_LEVEL", CallOp::BINDLESS_TEXTURE3D_SIZE_LEVEL)  //(bindless_array, index: uint, level: uint): uint3
+
+    	.value("BINDLESS_BUFFER_READ", CallOp::BINDLESS_BUFFER_READ)//(bindless_array, index: uint): expr->type()
+
+    	.value("MAKE_BOOL2", CallOp::MAKE_BOOL2)
+    	.value("MAKE_BOOL3", CallOp::MAKE_BOOL3)
+    	.value("MAKE_BOOL4", CallOp::MAKE_BOOL4)
+    	.value("MAKE_INT2", CallOp::MAKE_INT2)
+    	.value("MAKE_INT3", CallOp::MAKE_INT3)
+    	.value("MAKE_INT4", CallOp::MAKE_INT4)
+    	.value("MAKE_UINT2", CallOp::MAKE_UINT2)
+    	.value("MAKE_UINT3", CallOp::MAKE_UINT3)
+    	.value("MAKE_UINT4", CallOp::MAKE_UINT4)
+    	.value("MAKE_FLOAT2", CallOp::MAKE_FLOAT2)
+    	.value("MAKE_FLOAT3", CallOp::MAKE_FLOAT3)
+    	.value("MAKE_FLOAT4", CallOp::MAKE_FLOAT4)
+
+    	.value("MAKE_FLOAT2X2", CallOp::MAKE_FLOAT2X2)
+    	.value("MAKE_FLOAT3X3", CallOp::MAKE_FLOAT3X3)
+    	.value("MAKE_FLOAT4X4", CallOp::MAKE_FLOAT4X4)
+
+    	// optimization hints
+    	.value("ASSUME", CallOp::ASSUME)
+    	.value("UNREACHABLE", CallOp::UNREACHABLE)
+
+    	.value("INSTANCE_TO_WORLD_MATRIX", CallOp::INSTANCE_TO_WORLD_MATRIX)
+
+    	.value("TRACE_CLOSEST", CallOp::TRACE_CLOSEST)
+    	.value("TRACE_ANY", CallOp::TRACE_ANY);
+}
---
+++ src/py/export_vector3.cpp
@@ -0,0 +1,144 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/functional.h>
+#include <pybind11/stl.h>
+#include <luisa-compute.h>
+
+namespace py = pybind11;
+using namespace luisa::compute;
+
+#define LUISA_EXPORT_VECTOR3(T) \
+    py::class_<luisa::detail::VectorStorage<T, 3>>(m, "_vectorstorage_"#T"3"); \
+    py::class_<Vector<T,3>, luisa::detail::VectorStorage<T, 3>>(m, #T"3") \
+    	.def(py::init<T,T,T>()) \
+    	.def("__repr__", [](Vector<T,3>& self){return format(#T"3({},{},{})", self.x, self.y, self.z);}) \
+    	.def_readwrite("x", &Vector<T,3>::x) \
+    	.def_readwrite("y", &Vector<T,3>::y) \
+    	.def_readwrite("z", &Vector<T,3>::z) \
+		.def_property_readonly("xx", &Vector<T,3>::xx) \
+		.def_property_readonly("xy", &Vector<T,3>::xy) \
+		.def_property_readonly("xz", &Vector<T,3>::xz) \
+		.def_property_readonly("yx", &Vector<T,3>::yx) \
+		.def_property_readonly("yy", &Vector<T,3>::yy) \
+		.def_property_readonly("yz", &Vector<T,3>::yz) \
+		.def_property_readonly("zx", &Vector<T,3>::zx) \
+		.def_property_readonly("zy", &Vector<T,3>::zy) \
+		.def_property_readonly("zz", &Vector<T,3>::zz) \
+		.def_property_readonly("xxx", &Vector<T,3>::xxx) \
+		.def_property_readonly("xxy", &Vector<T,3>::xxy) \
+		.def_property_readonly("xxz", &Vector<T,3>::xxz) \
+		.def_property_readonly("xyx", &Vector<T,3>::xyx) \
+		.def_property_readonly("xyy", &Vector<T,3>::xyy) \
+		.def_property_readonly("xyz", &Vector<T,3>::xyz) \
+		.def_property_readonly("xzx", &Vector<T,3>::xzx) \
+		.def_property_readonly("xzy", &Vector<T,3>::xzy) \
+		.def_property_readonly("xzz", &Vector<T,3>::xzz) \
+		.def_property_readonly("yxx", &Vector<T,3>::yxx) \
+		.def_property_readonly("yxy", &Vector<T,3>::yxy) \
+		.def_property_readonly("yxz", &Vector<T,3>::yxz) \
+		.def_property_readonly("yyx", &Vector<T,3>::yyx) \
+		.def_property_readonly("yyy", &Vector<T,3>::yyy) \
+		.def_property_readonly("yyz", &Vector<T,3>::yyz) \
+		.def_property_readonly("yzx", &Vector<T,3>::yzx) \
+		.def_property_readonly("yzy", &Vector<T,3>::yzy) \
+		.def_property_readonly("yzz", &Vector<T,3>::yzz) \
+		.def_property_readonly("zxx", &Vector<T,3>::zxx) \
+		.def_property_readonly("zxy", &Vector<T,3>::zxy) \
+		.def_property_readonly("zxz", &Vector<T,3>::zxz) \
+		.def_property_readonly("zyx", &Vector<T,3>::zyx) \
+		.def_property_readonly("zyy", &Vector<T,3>::zyy) \
+		.def_property_readonly("zyz", &Vector<T,3>::zyz) \
+		.def_property_readonly("zzx", &Vector<T,3>::zzx) \
+		.def_property_readonly("zzy", &Vector<T,3>::zzy) \
+		.def_property_readonly("zzz", &Vector<T,3>::zzz) \
+		.def_property_readonly("xxxx", &Vector<T,3>::xxxx) \
+		.def_property_readonly("xxxy", &Vector<T,3>::xxxy) \
+		.def_property_readonly("xxxz", &Vector<T,3>::xxxz) \
+		.def_property_readonly("xxyx", &Vector<T,3>::xxyx) \
+		.def_property_readonly("xxyy", &Vector<T,3>::xxyy) \
+		.def_property_readonly("xxyz", &Vector<T,3>::xxyz) \
+		.def_property_readonly("xxzx", &Vector<T,3>::xxzx) \
+		.def_property_readonly("xxzy", &Vector<T,3>::xxzy) \
+		.def_property_readonly("xxzz", &Vector<T,3>::xxzz) \
+		.def_property_readonly("xyxx", &Vector<T,3>::xyxx) \
+		.def_property_readonly("xyxy", &Vector<T,3>::xyxy) \
+		.def_property_readonly("xyxz", &Vector<T,3>::xyxz) \
+		.def_property_readonly("xyyx", &Vector<T,3>::xyyx) \
+		.def_property_readonly("xyyy", &Vector<T,3>::xyyy) \
+		.def_property_readonly("xyyz", &Vector<T,3>::xyyz) \
+		.def_property_readonly("xyzx", &Vector<T,3>::xyzx) \
+		.def_property_readonly("xyzy", &Vector<T,3>::xyzy) \
+		.def_property_readonly("xyzz", &Vector<T,3>::xyzz) \
+		.def_property_readonly("xzxx", &Vector<T,3>::xzxx) \
+		.def_property_readonly("xzxy", &Vector<T,3>::xzxy) \
+		.def_property_readonly("xzxz", &Vector<T,3>::xzxz) \
+		.def_property_readonly("xzyx", &Vector<T,3>::xzyx) \
+		.def_property_readonly("xzyy", &Vector<T,3>::xzyy) \
+		.def_property_readonly("xzyz", &Vector<T,3>::xzyz) \
+		.def_property_readonly("xzzx", &Vector<T,3>::xzzx) \
+		.def_property_readonly("xzzy", &Vector<T,3>::xzzy) \
+		.def_property_readonly("xzzz", &Vector<T,3>::xzzz) \
+		.def_property_readonly("yxxx", &Vector<T,3>::yxxx) \
+		.def_property_readonly("yxxy", &Vector<T,3>::yxxy) \
+		.def_property_readonly("yxxz", &Vector<T,3>::yxxz) \
+		.def_property_readonly("yxyx", &Vector<T,3>::yxyx) \
+		.def_property_readonly("yxyy", &Vector<T,3>::yxyy) \
+		.def_property_readonly("yxyz", &Vector<T,3>::yxyz) \
+		.def_property_readonly("yxzx", &Vector<T,3>::yxzx) \
+		.def_property_readonly("yxzy", &Vector<T,3>::yxzy) \
+		.def_property_readonly("yxzz", &Vector<T,3>::yxzz) \
+		.def_property_readonly("yyxx", &Vector<T,3>::yyxx) \
+		.def_property_readonly("yyxy", &Vector<T,3>::yyxy) \
+		.def_property_readonly("yyxz", &Vector<T,3>::yyxz) \
+		.def_property_readonly("yyyx", &Vector<T,3>::yyyx) \
+		.def_property_readonly("yyyy", &Vector<T,3>::yyyy) \
+		.def_property_readonly("yyyz", &Vector<T,3>::yyyz) \
+		.def_property_readonly("yyzx", &Vector<T,3>::yyzx) \
+		.def_property_readonly("yyzy", &Vector<T,3>::yyzy) \
+		.def_property_readonly("yyzz", &Vector<T,3>::yyzz) \
+		.def_property_readonly("yzxx", &Vector<T,3>::yzxx) \
+		.def_property_readonly("yzxy", &Vector<T,3>::yzxy) \
+		.def_property_readonly("yzxz", &Vector<T,3>::yzxz) \
+		.def_property_readonly("yzyx", &Vector<T,3>::yzyx) \
+		.def_property_readonly("yzyy", &Vector<T,3>::yzyy) \
+		.def_property_readonly("yzyz", &Vector<T,3>::yzyz) \
+		.def_property_readonly("yzzx", &Vector<T,3>::yzzx) \
+		.def_property_readonly("yzzy", &Vector<T,3>::yzzy) \
+		.def_property_readonly("yzzz", &Vector<T,3>::yzzz) \
+		.def_property_readonly("zxxx", &Vector<T,3>::zxxx) \
+		.def_property_readonly("zxxy", &Vector<T,3>::zxxy) \
+		.def_property_readonly("zxxz", &Vector<T,3>::zxxz) \
+		.def_property_readonly("zxyx", &Vector<T,3>::zxyx) \
+		.def_property_readonly("zxyy", &Vector<T,3>::zxyy) \
+		.def_property_readonly("zxyz", &Vector<T,3>::zxyz) \
+		.def_property_readonly("zxzx", &Vector<T,3>::zxzx) \
+		.def_property_readonly("zxzy", &Vector<T,3>::zxzy) \
+		.def_property_readonly("zxzz", &Vector<T,3>::zxzz) \
+		.def_property_readonly("zyxx", &Vector<T,3>::zyxx) \
+		.def_property_readonly("zyxy", &Vector<T,3>::zyxy) \
+		.def_property_readonly("zyxz", &Vector<T,3>::zyxz) \
+		.def_property_readonly("zyyx", &Vector<T,3>::zyyx) \
+		.def_property_readonly("zyyy", &Vector<T,3>::zyyy) \
+		.def_property_readonly("zyyz", &Vector<T,3>::zyyz) \
+		.def_property_readonly("zyzx", &Vector<T,3>::zyzx) \
+		.def_property_readonly("zyzy", &Vector<T,3>::zyzy) \
+		.def_property_readonly("zyzz", &Vector<T,3>::zyzz) \
+		.def_property_readonly("zzxx", &Vector<T,3>::zzxx) \
+		.def_property_readonly("zzxy", &Vector<T,3>::zzxy) \
+		.def_property_readonly("zzxz", &Vector<T,3>::zzxz) \
+		.def_property_readonly("zzyx", &Vector<T,3>::zzyx) \
+		.def_property_readonly("zzyy", &Vector<T,3>::zzyy) \
+		.def_property_readonly("zzyz", &Vector<T,3>::zzyz) \
+		.def_property_readonly("zzzx", &Vector<T,3>::zzzx) \
+		.def_property_readonly("zzzy", &Vector<T,3>::zzzy) \
+		.def_property_readonly("zzzz", &Vector<T,3>::zzzz) \
+		; \
+	m.def("make_"#T"3", [](T a){return make_##T##3(a);}); \
+	m.def("make_"#T"3", [](T a, T b, T c){return make_##T##3(a,b,c);}); \
+	m.def("make_"#T"3", [](Vector<T,3> a){return make_##T##3(a);});
+
+void export_vector3(py::module &m) {
+	LUISA_EXPORT_VECTOR3(bool)
+	LUISA_EXPORT_VECTOR3(uint)
+	LUISA_EXPORT_VECTOR3(int)
+	LUISA_EXPORT_VECTOR3(float)
+}
