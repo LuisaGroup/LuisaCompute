3ca6e5ddc9913b996466a1afbc65032417e06970
LastMC <last_mc@126.com>
2022-05-02 19:16:42 +0800

    minor

--- src/backends/dx/Shader/ShaderCompiler.cpp
+++ src/backends/dx/Shader/ShaderCompiler.cpp
@@ -85,9 +85,9 @@ CompileResult DXShaderCompiler::CompileCompute(
     args.push_back_all(
         {L"-Qstrip_debug",
          L"-Qstrip_reflect",
+         L"/enable_unbounded_descriptor_tables",
          L"-all-resources-bound",
          L"-Gfa",
-         L"/enable_unbounded_descriptor_tables",
          L"-HV 2021"});
     if (optimize) {
         args.push_back(L"/O3");
@@ -110,6 +110,8 @@ CompileResult DXShaderCompiler::CompileRayTracing(
         {L"-Qstrip_debug",
          L"-Qstrip_reflect",
          L"/enable_unbounded_descriptor_tables",
+         L"-all-resources-bound",
+         L"-Gfa",
          L"-HV 2021"});
     if (optimize) {
         args.push_back(L"/O3");
