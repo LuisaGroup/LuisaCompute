ee02f88f1e69a10f6897e1906dcd82b882b0786e
bilibili <zhouzq18@yandex.com>
2022-05-07 21:00:44 +0800

    make pt less robust

--- src/py/pt99.py
+++ src/py/pt99.py
@@ -2,16 +2,17 @@
 
 import luisa
 from luisa.mathtypes import *
-from luisa.util import RandomSampler
 from math import pi
 from PIL import Image
-from luisa import make_ray
+from luisa.accel import make_ray
 
+# position of each vertex
 vertices = list(map(lambda x:float3(*x), [[-1,0,1], [1,0,1], [1,0,-1], [-1,0,-1], [-1,2,1], [-1,2,-1], [1,2,-1], [1,2,1], # room
     [.53,.6,.75], [.7,.6,.17], [.13,.6,0], [-.05,.6,.57], [.53,0,.75], [.7,0,.17], [.13,0,0], [-.05,0,.57], # short cube
     [-.53,1.2,.09], [.04,1.2,-.09], [-.14,1.2,-.67], [-.71,1.2,-.49], [-.53,0,.09], [.04,0,-.09], [-.14,0,-.67], [-.71,0,-.49], # tall cube
     [-.24,1.98,.16], [-.24,1.98,-.22], [.23,1.98,-.22], [.23,1.98,.16]])) # light
 
+# vertex indices of triangles in each mesh
 meshes = [[0,1,2,0,2,3], [4,5,6,4,6,7], [3,2,6,3,6,5], [2,1,7,2,7,6], [0,3,5,0,5,4], # room
          [8,9,10,8,10,11,15,11,10,15,10,14,12,8,11,12,11,15,13,9,8,13,8,12,14,10,9,14,9,13,12,13,14,12,14,15], # short cube
          [16,17,18,16,18,19,20,16,19,20,19,23,23,19,18,23,18,22,22,18,17,22,17,21,21,17,16,21,16,20,20,21,22,20,22,23], # tall cube
@@ -26,14 +27,13 @@ luisa.init()
 vertex_buffer = luisa.buffer(vertices)
 material_buffer = luisa.buffer(materials)
 triangle_buffers = list(map(luisa.buffer, meshes))
-mesh_table = luisa.bindless_array({i:buf for i,buf in enumerate(triangle_buffers)})
+triangle_buffer_table = luisa.bindless_array({i:buf for i,buf in enumerate(triangle_buffers)})
 accel = luisa.accel([luisa.Mesh(vertex_buffer, t) for t in triangle_buffers])
 
 @luisa.func
 def camera_ray(pixel):
     p = camera_pos + make_float3(pixel * tan(0.5 * fov), -1.0)
-    direction = normalize(p - camera_pos)
-    return make_ray(camera_pos, direction, 0.0, 1e30)
+    return make_ray(camera_pos, normalize(p - camera_pos), 0.0, 1e30)
 
 @luisa.func
 def cosine_sample_hemisphere(u, N):
@@ -45,8 +45,9 @@ def cosine_sample_hemisphere(u, N):
 
 @luisa.func
 def path_tracer(accum_image, frame_id, resolution):
-    sampler = RandomSampler(int3(dispatch_id().xy, frame_id)) # builtin RNG; the sobol sampler can be used instead to improve convergence
-    pixel = 2.0 / resolution * (float2(dispatch_id().xy) + sampler.next2f()) - 1.0
+    coord = dispatch_id().xy
+    sampler = luisa.RandomSampler(int3(coord, frame_id)) # builtin RNG; the sobol sampler can be used instead to improve convergence
+    pixel = 2.0 / resolution * (float2(coord) + sampler.next2f()) - 1.0
     ray = camera_ray(pixel * float2(1.0, -1.0))
 
     radiance = make_float3(0.0)
@@ -64,9 +65,9 @@ def path_tracer(accum_image, frame_id, resolution):
         hit = accel.trace_closest(ray)
         if hit.miss():
             break
-        v0_id = mesh_table.buffer_read(int, hit.inst, hit.prim * 3 + 0)
-        v1_id = mesh_table.buffer_read(int, hit.inst, hit.prim * 3 + 1)
-        v2_id = mesh_table.buffer_read(int, hit.inst, hit.prim * 3 + 2)
+        v0_id = triangle_buffer_table.buffer_read(int, hit.inst, hit.prim * 3 + 0)
+        v1_id = triangle_buffer_table.buffer_read(int, hit.inst, hit.prim * 3 + 1)
+        v2_id = triangle_buffer_table.buffer_read(int, hit.inst, hit.prim * 3 + 2)
         p0 = vertex_buffer.read(v0_id)
         p1 = vertex_buffer.read(v1_id)
         p2 = vertex_buffer.read(v2_id)
@@ -78,20 +79,17 @@ def path_tracer(accum_image, frame_id, resolution):
         albedo = material_buffer.read(hit.inst)
 
         if hit.inst == 7: # hit light
-            if depth == 0:
+            if depth == 0: # light is directly visible
                 radiance += light_emission
             break
 
         # sample light
         p_light = light_position + sampler.next() * light_u + sampler.next() * light_v
 
-        # offset to avoid self-intersection. or better, use offset_ray_origin(p,n)
-        pp = p + 1e-4 * n
-        pp_light = p_light + 1e-4 * light_normal
-        d_light = length(pp - pp_light)
-        wi_light = normalize(pp_light - pp)
-        shadow_ray = make_ray(pp, wi_light, 0.0, d_light)
-        occluded = accel.trace_any(shadow_ray)
+        # use eps to avoid self-intersection. or better, use offset_ray_origin(p,n)
+        d_light = length(p - p_light)
+        wi_light = normalize(p_light - p)
+        occluded = accel.trace_any(make_ray(p, wi_light, 1e-4, d_light - 2e-4))
         cos_wi_light = dot(wi_light, n)
         cos_light = -dot(light_normal, wi_light)
 
@@ -101,13 +99,10 @@ def path_tracer(accum_image, frame_id, resolution):
             radiance += beta * bsdf * light_emission / max(pdf_light, 1e-4)
 
         # sample BSDF
-        new_dir = cosine_sample_hemisphere(sampler.next2f(), n)
-        ray = make_ray(pp, new_dir, 0.0, 1e30)
+        ray = make_ray(p, cosine_sample_hemisphere(sampler.next2f(), n), 1e-4, 1e30)
         beta *= albedo
 
-    if any(isnan(radiance)):
-        radiance = float3(0.0)
-    accum_image.write(dispatch_id().xy, accum_image.read(dispatch_id().xy) + float4(clamp(radiance, 0.0, 30.0), 1.0))
+    accum_image.write(coord, accum_image.read(coord) + float4(clamp(radiance, 0.0, 30.0), 1.0))
 
 @luisa.func
 def linear_to_srgb(hdr_image, ldr_image, scale):
@@ -128,4 +123,5 @@ while gui.running():
         linear_to_srgb(accum_image, final_image, 1/frame_id, dispatch_size=(*res, 1))
         gui.set_image(final_image)
         gui.show()
+
 Image.fromarray(final_image.to(luisa.PixelStorage.BYTE4).numpy()).save("cornell.png")
