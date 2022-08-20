bcd5767ceb4241b7de8713b1879db1eaa51e35bf
Mike <mango@live.cn>
2022-08-20 15:51:56 +0800

    good

--- src/dsl/polymorphic.h
+++ src/dsl/polymorphic.h
@@ -44,11 +44,70 @@ public:
             LUISA_WARNING_WITH_LOCATION("No implementations registered.");
         }
         if (_impl.size() == 1u) {
-            f(_impl.front().get());
+            f(impl(0u));
         } else {
             detail::SwitchStmtBuilder{std::forward<Tag>(tag)} % [&] {
                 for (auto i = 0u; i < _impl.size(); i++) {
                     detail::SwitchCaseStmtBuilder{i} % [&f, this, i] { f(impl(i)); };
+                }
+                detail::SwitchDefaultStmtBuilder{} % unreachable;
+            };
+        }
+    }
+
+    template<typename Tag>
+        requires is_integral_expr_v<Tag>
+    void dispatch_range(Tag &&tag, uint lo, uint hi,
+                        const luisa::function<void(const T *)> &f) const noexcept {
+        LUISA_ASSERT(lo < hi, "Invalid polymorphic tag range [{}, {}).", lo, hi);
+        if (hi > _impl.size()) [[unlikely]] {
+            LUISA_WARNING_WITH_LOCATION(
+                "Out-of-bound polymorphic tag range [{}, {}). "
+                "Registered tag count: {}.", lo, hi, _impl.size());
+            hi = _impl.size();
+        }
+        if (hi == lo) [[unlikely]] {
+            LUISA_WARNING_WITH_LOCATION("No implementations registered.");
+        }
+        if (hi == lo + 1u) {// only one implementation
+            f(impl(lo));
+        } else {
+            detail::SwitchStmtBuilder{std::forward<Tag>(tag)} % [&] {
+                for (auto i = lo; i < hi; i++) {
+                    detail::SwitchCaseStmtBuilder{i} % [&f, this, i] { f(impl(i)); };
+                }
+                detail::SwitchDefaultStmtBuilder{} % unreachable;
+            };
+        }
+    }
+
+    template<typename Tag, typename Group>
+        requires is_integral_expr_v<Tag>
+    void dispatch_group(Tag &&tag, const Group &group,
+                        const luisa::function<void(const T *)> &f) const noexcept {
+        luisa::vector<uint> tags;
+        tags.reserve(std::size(group));
+        for (auto &&t : group) {
+            if (t < _impl.size()) {
+                tags.emplace_back(t);
+            } else {
+                LUISA_WARNING_WITH_LOCATION(
+                    "Out-of-bound polymorphic tag {}. "
+                    "Registered tag count: {}.", t, _impl.size());
+            }
+        }
+        std::sort(tags.begin(), tags.end());
+        tags.erase(std::unique(tags.begin(), tags.end()), tags.end());
+        if (tags.empty()) [[unlikely]] {
+            LUISA_WARNING_WITH_LOCATION("No implementations registered.");
+        }
+        LUISA_ASSERT(group.size() > 0, "Empty polymorphic tag group.");
+        if (tags.size() == 1u) {
+            f(impl(tags.front()));
+        } else {
+            detail::SwitchStmtBuilder{std::forward<Tag>(tag)} % [&] {
+                for (auto t : tags) {
+                    detail::SwitchCaseStmtBuilder{t} % [&f, this, t] { f(impl(t)); };
                 }
                 detail::SwitchDefaultStmtBuilder{} % unreachable;
             };
