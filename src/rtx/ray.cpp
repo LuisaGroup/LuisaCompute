5fb16bd9c9a74af1b53173c7ef5d68eaa0de8c3b
Mike Smith <mango@live.cn>
2022-10-06 22:00:37 +0800

    minor

--- src/rtx/ray.cpp
+++ src/rtx/ray.cpp
@@ -16,12 +16,15 @@ Var<Ray> make_ray(Expr<float3> origin, Expr<float3> direction) noexcept {
 }
 
 Float3 offset_ray_origin(Expr<float3> p, Expr<float3> n) noexcept {
-    constexpr auto origin = 1.0f / 32.0f;
-    constexpr auto float_scale = 1.0f / 65536.0f;
-    constexpr auto int_scale = 256.0f;
-    auto of_i = make_int3(int_scale * n);
-    auto p_i = as<float3>(as<int3>(p) + ite(p < 0.0f, -of_i, of_i));
-    return ite(abs(p) < origin, p + float_scale * n, p_i);
+    static Callable impl = [](Float3 p, Float3 n) noexcept {
+        constexpr auto origin = 1.0f / 32.0f;
+        constexpr auto float_scale = 1.0f / 65536.0f;
+        constexpr auto int_scale = 256.0f;
+        auto of_i = make_int3(int_scale * n);
+        auto p_i = as<float3>(as<int3>(p) + ite(p < 0.0f, -of_i, of_i));
+        return ite(abs(p) < origin, p + float_scale * n, p_i);
+    };
+    return impl(p, n);
 }
 
 Float3 offset_ray_origin(Expr<float3> p, Expr<float3> n, Expr<float3> w) noexcept {
