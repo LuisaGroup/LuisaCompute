45fb39ed5399445389f0d25625bfd86ed51fc334
Xiaochun Tong <xtong@uwaterloo.ca>
2022-12-08 11:58:52 -0500

    fix bindgen

--- src/api/common.h
+++ src/api/common.h
@@ -1,7 +1,12 @@
 #pragma once
 #include <core/platform.h>
+#ifdef __cplusplus
 #include <cstdint>
 #include <cstddef>
+#else
+#include <stdint.h>
+#include <stddef.h>
+#endif
 #include <rust/luisa_compute_api_types/bindings.h>
 #ifdef __cplusplus
 #include <rust/luisa_compute_api_types/bindings.hpp>
