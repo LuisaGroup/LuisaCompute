29ee0c266a6f2990acd0165518f3693fe88571ad
Mike Smith <mango@live.cn>
2022-12-16 20:09:09 +0800

    minor

--- src/core/stl.h
+++ src/core/stl.h
@@ -195,13 +195,13 @@ template<typename K, typename V,
          typename Hash = hash<K>,
          typename Eq = equal_to<>,
          typename Allocator = luisa::allocator<std::pair<const K, V>>>
-using unordered_map = tsl::robin_map<K, V, Hash, Eq, Allocator, std::vector>;
+using unordered_map = tsl::robin_map<K, V, Hash, Eq, Allocator>;
 
 template<typename K,
          typename Hash = hash<K>,
          typename Eq = equal_to<>,
          typename Allocator = luisa::allocator<const K>>
-using unordered_set = tsl::robin_set<K, Hash, Eq, Allocator, std::vector>;
+using unordered_set = tsl::robin_set<K, Hash, Eq, Allocator>;
 #else
 using std::unordered_map;
 using std::unordered_set;
