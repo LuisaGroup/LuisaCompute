0a839fd245320588906b1fa5ce14ca891c41f1b1
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2022-12-23 20:47:06 +0800

    fixed format.h compile error

--- src/core/stl/format.h
+++ src/core/stl/format.h
@@ -12,23 +12,18 @@ namespace luisa {
 #ifndef FMT_STRING
 #define FMT_STRING(...) __VA_ARGS__
 #endif
-
-using fmt::format_to;
-
 template<typename String, typename Format, typename... Args>
 [[nodiscard]] inline auto format(Format &&f, Args &&...args) noexcept {
     using char_type = typename String::value_type;
     using memory_buffer = fmt::basic_memory_buffer<char_type, fmt::inline_buffer_size, luisa::allocator<char_type>>;
     memory_buffer buffer;
-    format_to(std::back_inserter(buffer), std::forward<Format>(f), std::forward<Args>(args)...);
+    fmt::format_to(std::back_inserter(buffer), std::forward<Format>(f), std::forward<Args>(args)...);
     return String{buffer.data(), buffer.size()};
 }
-
 template<typename Format, typename... Args>
 [[nodiscard]] inline auto format(Format &&f, Args &&...args) noexcept {
     return format<luisa::string>(std::forward<Format>(f), std::forward<Args>(args)...);
 }
-
 [[nodiscard]] inline auto hash_to_string(uint64_t hash) noexcept {
     return luisa::format("{:016X}", hash);
 }
