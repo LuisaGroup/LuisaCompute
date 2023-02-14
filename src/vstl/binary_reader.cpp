c27dddea91f0d004625091cb8c3ff3dfe16474a3
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-02-14 12:18:54 +0800

    Update binary_reader.cpp

--- src/vstl/binary_reader.cpp
+++ src/vstl/binary_reader.cpp
@@ -2,9 +2,15 @@
 #ifdef _WIN32
 #define VSTD_FSEEK _fseeki64_nolock
 #define VSTD_FTELL _ftelli64_nolock
+#define VSTD_FREAD _fread_nolock
+#define VSTD_FWRITE _fwrite_nolock
+#define VSTD_FCLOSE _fclose_nolock
 #else
 #define VSTD_FSEEK fseek
 #define VSTD_FTELL ftell
+#define VSTD_FREAD fread
+#define VSTD_FWRITE fwrite
+#define VSTD_FCLOSE fclose
 #endif
 BinaryReader::BinaryReader(vstd::string const &path) {
     currentPos = 0;
@@ -28,7 +34,7 @@ void BinaryReader::Read(void *ptr, uint64 len) {
     currentPos = targetEnd;
     if (len == 0) return;
     VSTD_FSEEK(ifs, lastPos, SEEK_SET);
-    fread(ptr, len, 1, ifs);
+    VSTD_FREAD(ptr, len, 1, ifs);
 }
 vstd::string BinaryReader::ReadToString() {
     if (!isAvaliable) return {};
@@ -44,7 +50,7 @@ vstd::string BinaryReader::ReadToString() {
     vstd::string str;
     str.resize(len);
     VSTD_FSEEK(ifs, lastPos, SEEK_SET);
-    fread(str.data(), len, 1, ifs);
+    VSTD_FREAD(str.data(), len, 1, ifs);
     return str;
 }
 
@@ -67,12 +73,12 @@ luisa::vector<uint8_t> BinaryReader::Read(bool addNullEnd) {
     result.resize(addNullEnd ? (len + 1) : len);
     currentPos = targetEnd;
     VSTD_FSEEK(ifs, lastPos, SEEK_SET);
-    fread(result.data(), len, 1, ifs);
+    VSTD_FREAD(result.data(), len, 1, ifs);
     return result;
 }
 BinaryReader::~BinaryReader() {
     if (!isAvaliable) return;
-    fclose(ifs);
+    VSTD_FCLOSE(ifs);
 }
 
 #undef VSTD_FSEEK
