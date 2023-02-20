80dc50af9aae569826d71d820486c91387fc5f47
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-02-20 11:09:09 +0800

    fix

--- src/runtime/raster/raster_kernel.h
+++ src/runtime/raster/raster_kernel.h
@@ -26,7 +26,7 @@ public:
     RasterKernel(VertexKernel const &vert, PixelKernel const &pixel) noexcept {
         Type const *v2pType = Type::template of<VertRet>();
         // Structure's first element must be float4 as position
-        assert((v2pType->is_vector() && v2pType->element()->tag() == Type::Tag::FLOAT && v2pType->dimension() == 4) || (v2pType->is_structure() && v2pType->members().size() >= 1 && v2pType->members()[0]->is_vector() && v2pType->members()[0]->element()->tag() == Type::Tag::FLOAT && v2pType->members()[0]->dimension() == 4));
+        assert((v2pType->is_vector() && v2pType->element()->tag() == Type::Tag::FLOAT32 && v2pType->dimension() == 4) || (v2pType->is_structure() && v2pType->members().size() >= 1 && v2pType->members()[0]->is_vector() && v2pType->members()[0]->element()->tag() == Type::Tag::FLOA32 && v2pType->members()[0]->dimension() == 4));
         _vert = vert.function_builder();
         _pixel = pixel.function_builder();
     }
