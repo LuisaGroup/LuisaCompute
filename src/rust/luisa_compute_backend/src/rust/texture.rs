aa90c3a3a41da3f5cd03fcd4b21f17f0e8f98212
Xiaochun Tong <xtong@uwaterloo.ca>
2023-02-26 02:54:31 -0500

    working on texture

--- src/rust/luisa_compute_backend/src/rust/texture.rs
+++ src/rust/luisa_compute_backend/src/rust/texture.rs
@@ -53,8 +53,108 @@ impl TextureImpl {
             storage,
         }
     }
+    pub(crate) fn view(&self, level: u8) -> TextureView {
+        let offset = self.mip_offsets[level as usize];
+        let size = [
+            self.size[0] >> level,
+            self.size[1] >> level,
+            self.size[2] >> level,
+        ];
+        unsafe {
+            TextureView {
+                data: self.data.as_ptr().add(offset) as *mut u32,
+                size,
+                pixel_u32_count: self.pixel_u32_count,
+            }
+        }
+    }
 }
-struct TextureView {
-    data:*mut u32,
+pub(crate) struct TextureView {
+    data: *mut u32,
     size: [u32; 3],
+    pixel_u32_count: usize,
+}
+
+impl TextureView {
+    #[inline]
+    pub(crate) fn get_pixel_2d(&self, x: u32, y: u32) -> *mut u32 {
+        let block_x = x / BLOCK_SIZE as u32;
+        let block_y = y / BLOCK_SIZE as u32;
+        let grid_width = (self.size[0] + BLOCK_SIZE as u32 - 1) / BLOCK_SIZE as u32;
+        let block_idx = block_x + block_y * grid_width;
+        let pixel_x = x % BLOCK_SIZE as u32;
+        let pixel_y = y % BLOCK_SIZE as u32;
+        let pixel_idx =
+            block_idx * (BLOCK_SIZE * BLOCK_SIZE) as u32 + pixel_x + pixel_y * BLOCK_SIZE as u32;
+        unsafe { self.data.add(pixel_idx as usize * self.pixel_u32_count) }
+    }
+    #[inline]
+    pub(crate) fn get_pixel_3d(&self, x: u32, y: u32, z: u32) -> *mut u32 {
+        let block_x = x / BLOCK_SIZE as u32;
+        let block_y = y / BLOCK_SIZE as u32;
+        let block_z = z / BLOCK_SIZE as u32;
+        let grid_width = (self.size[0] + BLOCK_SIZE as u32 - 1) / BLOCK_SIZE as u32;
+        let grid_height = (self.size[1] + BLOCK_SIZE as u32 - 1) / BLOCK_SIZE as u32;
+        let block_idx = block_x + block_y * grid_width + block_z * grid_width * grid_height;
+        let pixel_x = x % BLOCK_SIZE as u32;
+        let pixel_y = y % BLOCK_SIZE as u32;
+        let pixel_z = z % BLOCK_SIZE as u32;
+        let pixel_idx = block_idx * (BLOCK_SIZE * BLOCK_SIZE * BLOCK_SIZE) as u32
+            + pixel_x
+            + pixel_y * BLOCK_SIZE as u32
+            + pixel_z * BLOCK_SIZE as u32 * BLOCK_SIZE as u32;
+        unsafe { self.data.add(pixel_idx as usize * self.pixel_u32_count) }
+    }
+    #[inline]
+    pub(crate) fn copy_from_2d(&self, mut data: *const u32) {
+        for y in 0..self.size[1] {
+            for x in 0..self.size[0] {
+                let dst = self.get_pixel_2d(x, y);
+                unsafe {
+                    std::ptr::copy_nonoverlapping(data, dst, self.pixel_u32_count);
+                    data = data.add(self.pixel_u32_count);
+                }
+            }
+        }
+    }
+    #[inline]
+    pub(crate) fn copy_from_3d(&self, mut data: *const u32) {
+        for z in 0..self.size[2] {
+            for y in 0..self.size[1] {
+                for x in 0..self.size[0] {
+                    let dst = self.get_pixel_3d(x, y, z);
+                    unsafe {
+                        std::ptr::copy_nonoverlapping(data, dst, self.pixel_u32_count);
+                        data = data.add(self.pixel_u32_count);
+                    }
+                }
+            }
+        }
+    }
+    #[inline]
+    pub(crate) fn copy_to_2d(&self, mut data: *mut u32) {
+        for y in 0..self.size[1] {
+            for x in 0..self.size[0] {
+                let src = self.get_pixel_2d(x, y);
+                unsafe {
+                    std::ptr::copy_nonoverlapping(src, data, self.pixel_u32_count);
+                    data = data.add(self.pixel_u32_count);
+                }
+            }
+        }
+    }
+    #[inline]
+    pub(crate) fn copy_to_3d(&self, mut data: *mut u32) {
+        for z in 0..self.size[2] {
+            for y in 0..self.size[1] {
+                for x in 0..self.size[0] {
+                    let src = self.get_pixel_3d(x, y, z);
+                    unsafe {
+                        std::ptr::copy_nonoverlapping(src, data, self.pixel_u32_count);
+                        data = data.add(self.pixel_u32_count);
+                    }
+                }
+            }
+        }
+    }
 }
