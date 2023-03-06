b575b153e851ee395d5883f0e0496788c575d5e7
Mike <Mango@Live.cn>
2023-03-06 11:54:30 +0800

    copy function return in ray query api

--- src/dsl/rtx/ray_query.cpp
+++ src/dsl/rtx/ray_query.cpp
@@ -37,16 +37,16 @@ RayQuery::RayQuery(const CallExpr *func) noexcept {
 //     return def<Hit>(func_builder->call(Type::of<Hit>(), CallOp::RAY_QUERY_COMMITTED_HIT, {_expr}));
 // }
 Var<bool> RayQuery::proceed() const noexcept {
-    return Var<bool>{detail::FunctionBuilder::current()->call(Type::of<bool>(), CallOp::RAY_QUERY_PROCEED, {_expr})};
+    return def<bool>(detail::FunctionBuilder::current()->call(Type::of<bool>(), CallOp::RAY_QUERY_PROCEED, {_expr}));
 }
 Var<Hit> RayQuery::triangle_candidate() const noexcept {
-    return Var<Hit>{detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_TRIANGLE_CANDIDATE_HIT, {_expr})};
+    return def<Hit>(detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_TRIANGLE_CANDIDATE_HIT, {_expr}));
 }
 Var<Hit> RayQuery::procedural_candidate() const noexcept {
-    return Var<Hit>{detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_PROCEDURAL_CANDIDATE_HIT, {_expr})};
+    return def<Hit>(detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_PROCEDURAL_CANDIDATE_HIT, {_expr}));
 }
 Var<bool> RayQuery::is_candidate_triangle() const noexcept {
-    return Var<bool>{detail::FunctionBuilder::current()->call(Type::of<bool>(), CallOp::RAY_QUERY_IS_CANDIDATE_TRIANGLE, {_expr})};
+    return def<bool>(detail::FunctionBuilder::current()->call(Type::of<bool>(), CallOp::RAY_QUERY_IS_CANDIDATE_TRIANGLE, {_expr}));
 }
 Var<bool> RayQuery::is_candidate_procedural() const noexcept {
     return !is_candidate_triangle();
@@ -58,6 +58,6 @@ void RayQuery::commit_procedural(Expr<float> distance) const noexcept {
     detail::FunctionBuilder::current()->call(CallOp::RAY_QUERY_COMMIT_PROCEDURAL, {_expr, distance.expression()});
 }
 Var<Hit> RayQuery::committed_hit() const noexcept {
-    return Var<Hit>{detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_COMMITTED_HIT, {_expr})};
+    return def<Hit>(detail::FunctionBuilder::current()->call(Type::of<Hit>(), CallOp::RAY_QUERY_COMMITTED_HIT, {_expr}));
 }
 }// namespace luisa::compute
