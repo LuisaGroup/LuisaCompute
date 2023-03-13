dedec5cbab687ccecbd0c452ea54e24da6d561ec
Mike <mango@live.cn>
2023-03-13 17:35:56 +0800

    fix

--- src/core/binary_io.cpp
+++ src/core/binary_io.cpp
@@ -7,7 +7,7 @@
 
 namespace luisa {
 
-void BinaryStringStream::read(luisa::span<std::byte> dst) {
+void BinaryStringStream::read(luisa::span<std::byte> dst) noexcept {
     auto size = std::min(dst.size(), _data.size() - _pos);
     std::memcpy(dst.data(), _data.data() + _pos, size);
     _pos += size;
@@ -29,28 +29,54 @@ void BinaryStringStream::read(luisa::span<std::byte> dst) {
 #define LUISA_FCLOSE fclose
 #endif
 
-BinaryFileStream::BinaryFileStream(luisa::string_view path) noexcept {
-    _file = LUISA_FOPEN(path.data(), "rb");
+BinaryFileStream::BinaryFileStream(const luisa::string &path) noexcept {
+    _file = LUISA_FOPEN(path.c_str(), "rb");
     if (_file == nullptr) { LUISA_WARNING_WITH_LOCATION(
         "Failed to create file stream for file '{}'.", path); }
-}
-
-BinaryFileStream::~BinaryFileStream() noexcept {
-    if (_file) { LUISA_FCLOSE(_file); }
-}
-
-size_t BinaryFileStream::length() const {
-    auto pos = LUISA_FTELL(_file);
     LUISA_FSEEK(_file, 0, SEEK_END);
-    auto length = LUISA_FTELL(_file);
-    LUISA_FSEEK(_file, pos, SEEK_SET);
-    return length;
+    _length = LUISA_FTELL(_file);
+    LUISA_FSEEK(_file, 0, SEEK_SET);
 }
 
-size_t BinaryFileStream::pos() const { return LUISA_FTELL(_file); }
+BinaryFileStream::~BinaryFileStream() noexcept { close(); }
 
-void BinaryFileStream::read(luisa::span<std::byte> dst) {
-    LUISA_FREAD(dst.data(), 1, dst.size(), _file);
+BinaryFileStream::BinaryFileStream(BinaryFileStream &&another) noexcept
+    : _file{another._file},
+      _length{another._length},
+      _pos{another._pos} {
+    another._file = nullptr;
+    another._length = 0;
+    another._pos = 0;
+}
+
+BinaryFileStream &BinaryFileStream::operator=(BinaryFileStream &&rhs) noexcept {
+    if (&rhs != this) {
+        close();
+        _file = rhs._file;
+        _length = rhs._length;
+        _pos = rhs._pos;
+        rhs._file = nullptr;
+        rhs._length = 0;
+        rhs._pos = 0;
+    }
+    return *this;
+}
+
+void BinaryFileStream::read(luisa::span<std::byte> dst) noexcept {
+    if (!valid()) {
+        LUISA_WARNING_WITH_LOCATION(
+            "Failed to read from invalid file stream.");
+        return;
+    }
+    auto size = std::min(dst.size(), _length - _pos);
+    LUISA_FREAD(dst.data(), 1, size, _file);
+    _pos += size;
+}
+
+void BinaryFileStream::close() noexcept {
+    if (_file) { LUISA_FCLOSE(_file); }
+    _length = 0;
+    _pos = 0;
 }
 
 #undef LUISA_FOPEN
