a44aa7ee2684c35ce85764800f52cac60c748bee
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-03-17 17:11:58 +0800

    Update test-buffer.py

--- src/tests/python/test-buffer.py
+++ src/tests/python/test-buffer.py
@@ -8,14 +8,21 @@ buffer = Buffer(32, MyStructType)
 print("buffer byte size(should be 32 * 32 = 1024): " + str(buffer.bytesize))
 # def get_negative_number_with_arg(buffer, atomic_buffer):
 
-
+# ConstantFlag = False
 @func
-def get_negative_number():
+def kernel():
     index = dispatch_id().x
     tmp = buffer.read(index)
     tmp.my_v0 = sin(tmp.my_v0)
     tmp.my_v1 = cos(tmp.my_v1)
     buffer.write(index, tmp)
+    # "Macro Branch" showoff here
+    ConstantFlag = ((1 + 1) == 3) # ConstantFlag is an always-false constant value, simple calculations supported
+    # this "if ConstantFlag" branch will be eliminated before codegen, so this code block can be ridiculously illegal!
+    
+    # if True:
+    if ConstantFlag:
+        ridiculous.wtf_is_this(nothing)
 
 
 array = np.zeros([32 * 8], dtype=np.float32)
@@ -23,7 +30,7 @@ buffer.copy_from(array)
 # write array after "copy_from" command is fine here, all commands is scheduled in queue before call execute()
 for i in range(len(array)):
     array[i] = i
-get_negative_number(dispatch_size=(32, 1, 1))
+kernel(dispatch_size=(32, 1, 1))
 # all commands start running when execute() is called
 execute()
 # get_negative_number_with_arg(buffer, atomic_buffer, dispatch_size=(32,1,1))
