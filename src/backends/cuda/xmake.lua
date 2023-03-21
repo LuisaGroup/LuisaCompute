fef507d7be58b43d68bec193ef585af1e03f9a2c
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-03-21 10:47:19 +0800

    add cuda switch

--- src/backends/cuda/xmake.lua
+++ src/backends/cuda/xmake.lua
@@ -1,14 +1,21 @@
 target("lc-backend-cuda")
 _config_project({
 	project_kind = "shared",
-	batch_size = 8
+	batch_size = 4
 })
+on_load(function(target)
+	local cuda_path = os.getenv("CUDA_PATH")
+	if cuda_path then
+		target:add("includedirs", cuda_path .. "/include")
+		target:add("linkdirs", cuda_path .. "/lib/x64")
+		target:add("links", "cuda", "nvrtc")
+	else
+		target:set("enabled", false)
+	end
+end)
 add_deps("lc-runtime", "lc-backend")
 add_files("**.cpp")
 -- add_includedirs("#")
-add_includedirs("$(env CUDA_PATH)/include")
-add_linkdirs("$(env CUDA_PATH)/lib/x64")
-add_links("cuda", "nvrtc")
 after_build(function(target)
 	local binDir = target:targetdir()
 	os.cp("src/backends/cuda/cuda_builtin", path.join(binDir, ".data/"))
