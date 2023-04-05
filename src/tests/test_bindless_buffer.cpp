2524977d5cc82ebe892397c1ce56aa4bab292d58
Mike <mango@live.cn>
2023-04-05 15:26:11 +0800

    update bindless example

--- src/tests/test_bindless_buffer.cpp
+++ src/tests/test_bindless_buffer.cpp
@@ -1,107 +1,53 @@
-//
-// Created by Mike Smith on 2021/4/6.
-//
-
-#include <stb/stb_image.h>
-#include <stb/stb_image_write.h>
-#include <stb/stb_image_resize.h>
-
-#include <core/logging.h>
-#include <runtime/context.h>
-#include <runtime/device.h>
-#include <runtime/stream.h>
-#include <runtime/event.h>
-#include <dsl/syntax.h>
+#include <luisa-compute.h>
+#include <dsl/sugar.h>
+#include <gui/window.h>
 
 using namespace luisa;
 using namespace luisa::compute;
 
+// contributed by @swifly in issue #67
 int main(int argc, char *argv[]) {
 
-    log_level_verbose();
-
     Context context{argv[0]};
-    if(argc <= 1){
+    if (argc <= 1) {
         LUISA_INFO("Usage: {} <backend>. <backend>: cuda, dx, ispc, metal", argv[0]);
         exit(1);
     }
     auto device = context.create_device(argv[1]);
 
-    Kernel2D clear_image_kernel = [](BufferVar<uint> image) noexcept {
+    constexpr auto resolution = make_uint2(1280, 720);
+
+    auto stream = device.create_stream(StreamTag::GRAPHICS);
+    auto device_image1 = device.create_image<float>(PixelStorage::BYTE4, resolution);
+    auto bdls = device.create_bindless_array();
+    auto buffer = device.create_buffer<float4>(4);
+    std::vector<float4> a{4};
+    a[0] = {1, 0, 0, 1};
+    a[1] = {0, 1, 0, 1};
+    a[2] = {0, 0, 1, 1};
+    a[3] = {1, 1, 1, 1};
+    stream << buffer.copy_from(a.data()) << synchronize();
+    bdls.emplace_on_update(0, buffer);
+    stream << bdls.update() << synchronize();
+
+    Kernel2D kernel = [&](Float time) {
         Var coord = dispatch_id().xy();
-        Var index = coord.y * dispatch_size_x() + coord.x;
-        image.write(index, 0u);
+        UInt i2 = ((coord.x + cast<uint>(time)) / 16 % 4);
+        auto vertexArray = bdls->buffer<float4>(0);
+        Float4 p = vertexArray.read(i2);
+        device_image1->write(coord, make_float4(p));
     };
+    auto s = device.compile(kernel);
 
-    // Callable sample = [](BindlessVar heap, Float2 uv, Float mip) noexcept {
-    //     return heap.tex2d(0u).sample(uv, make_float2(), make_float2());
-    // };
+    Window window{"Display", resolution};
 
-    Callable read_buffer = [](BindlessVar heap, UInt index) noexcept {
-        UInt x = 0;
-        return heap.buffer<uint>(0u).read(index);
-    };
-    // auto useless_shader = device.compile(useless_kernel);
-
-    Kernel2D fill_image_kernel = [&](BindlessVar heap, BufferUInt image) noexcept {
-        // Var coord = dispatch_id().xy();
-        // Var index = coord.x;
-        auto index = dispatch_y() * dispatch_size_x() + dispatch_x();
-        // Var uv = make_float2(coord) / make_float2(dispatch_size().xy());
-        // Var r = length(uv - 0.5f);
-        // Var t = log(sin(sqrt(r) * 100.0f - constants::pi_over_two) + 2.0f);
-        // image.write(index, 0u);
-        image.write(index, read_buffer(heap, index));
-    };
-
-    auto clear_image = device.compile(clear_image_kernel);
-    auto fill_image = device.compile(fill_image_kernel);
-
-    auto heap = device.create_bindless_array();
-    auto image_width = 0;
-    auto image_height = 0;
-    auto image_channels = 0;
-    auto image_pixels = stbi_load("logo.png", &image_width, &image_height, &image_channels, 4);
-    auto texture = device.create_buffer<uint>(image_width * image_height);
-    auto device_image = device.create_buffer<uint>(1024u * 1024u);
-    std::vector<uint8_t> host_image(1024u * 1024u * 4u);
-
-    // auto event = device.create_event();
-    auto stream = device.create_stream();
-    // auto upload_stream = device.create_stream();
-
-    // std::vector<uint8_t> mipmaps(image_width * image_height * 4u);
-    // auto in_pixels = image_pixels;
-    // auto out_pixels = mipmaps.data();
-
-    // generate mip-maps
-    auto cmd = CommandList::create();
-    cmd << heap.emplace_on_update(0, texture).update()
-        << texture.copy_from(image_pixels);
-    // for (auto i = 1u; i < texture.mip_levels(); i++) {
-    //     auto half_w = std::max(image_width / 2, 1);
-    //     auto half_h = std::max(image_height / 2, 1);
-    //     stbir_resize_uint8_srgb_edgemode(
-    //         in_pixels,
-    //         image_width, image_height, 0,
-    //         out_pixels,
-    //         half_w, half_h, 0,
-    //         4, STBIR_ALPHA_CHANNEL_NONE, 0,
-    //         STBIR_EDGE_REFLECT);
-    //     image_width = half_w;
-    //     image_height = half_h;
-    //     stbi_write_png(fmt::format("level-{}.png", i).c_str(), image_width, image_height, 4, out_pixels, 0);
-    //     cmd << texture.view(i).copy_from(out_pixels);
-    //     in_pixels = out_pixels;
-    //     out_pixels += image_width * image_height * 4u;
-    // }
-    stream << cmd.commit()
-           << clear_image(device_image).dispatch(1024u, 1024u)
-        //    << event.wait()
-           << fill_image(heap, device_image).dispatch(1024u, 1024u)
-           << device_image.copy_to(host_image.data())
-        //    << event.signal()
-           << synchronize();
-
-    stbi_write_png("result.png", 1024u, 1024u, 4u, host_image.data(), 0u);
+    auto swapchain = device.create_swapchain(
+        window.native_handle(), stream, resolution, false);
+    Clock clk;
+    while (!window.should_close()) {
+        stream << s(static_cast<float>(clk.toc() * .05f))
+                      .dispatch(1280, 720)
+               << swapchain.present(device_image1);
+        window.pool_event();
+    }
 }
