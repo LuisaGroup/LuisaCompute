9f8fc734990f3aded96f0664abdf71e8cfe558aa
Mike <mango@live.cn>
2023-04-23 18:56:24 +0800

    update device lib

--- src/backends/cuda/cuda_builtin_embedded.h
+++ src/backends/cuda/cuda_builtin_embedded.h
@@ -2,4 +2,4 @@
 
 extern "C" const char luisa_cuda_builtin_cuda_builtin_kernels[5490];
 extern "C" const char luisa_cuda_builtin_cuda_device_math[405238];
-extern "C" const char luisa_cuda_builtin_cuda_device_resource[78588];
+extern "C" const char luisa_cuda_builtin_cuda_device_resource[78599];
