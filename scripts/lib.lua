03d520f201985a6176fdb55ece1230132327f18a
Maxwell Geng <maxwellgeng@outlook.com>
2023-04-25 11:37:05 +0800

    minor

--- scripts/lib.lua
+++ scripts/lib.lua
@@ -15,10 +15,11 @@ function string_contains(str, sub_str)
 end
 local libc = import("core/base/libc")
 local bytes = import("core/base/bytes")
-local function _to_string(self)
+local _string_builder = {}
+function _string_builder:to_string()
 	return libc.strndup(self._ptr + 1, self._size)
 end
-local function add_capacity(self, s)
+function _string_builder:add_capacity(s)
 	local size = s + self._size
 	local capa = self._capacity
 	if capa >= size then
@@ -34,7 +35,7 @@ local function add_capacity(self, s)
 	self._ptr = new_ptr - 1
 	self._capacity = capa
 end
-local function _reserve(self, s)
+function _string_builder:reserve(s)
 	local capa = self._capacity
 	if capa >= s then
 		return
@@ -52,79 +53,65 @@ local function max(a, b)
 	end
 	return b
 end
-local function _add(self, str)
+function _string_builder:add(str)
 	if #str == 0 then
 		return
 	end
-	add_capacity(self, #str)
+	self:add_capacity(#str)
 	local ptr = self._ptr + self._size + 1
 	libc.memcpy(ptr, libc.dataptr(str), #str)
 	self._size = self._size + #str
 	return self
 end
-local function _add_char(self, c)
-	add_capacity(self, 1)
+function _string_builder:add_char(c)
+	self:add_capacity(1)
 	self._size = self._size + 1
 	libc.setbyte(self._ptr, self._size, c)
 	return self
 end
-local function _dispose(self)
+function _string_builder:dispose()
 	if self._ptr ~= -1 then
 		libc.free(self._ptr + 1)
 		self._ptr = -1
 	end
 end
-local function _write_to(self, path)
+function _string_builder:write_to(path)
 	local f = io.open(path, "wb")
 	f:write(self)
 	f:close()
 end
-local function _get(self, i)
+function _string_builder:get(i)
 	return libc.byteof(self._ptr, i)
 end
-local function _set(self, i, v)
+function _string_builder:set(i, v)
 	return libc.setbyte(self._ptr, i, v)
 end
-local function _erase(self, i)
+function _string_builder:erase(i)
 	self._size = max(self._size - i, 1)
 end
+function _string_builder:size()
+	return self._size
+end
+function _string_builder:capacity()
+	return self._capacity
+end
+function _string_builder:caddr()
+	return self._ptr + 1
+end
 function StringBuilder(str)
-	local inst
+	local inst = table.inherit(_string_builder)
 	if str then
 		local capa = max(32, #str)
 		local addr = libc.malloc(capa)
-		inst = {
-			_size = #str,
-			_capacity = capa,
-			_ptr = addr - 1
-		}
+		inst._size = #str
+		inst._capacity = capa
+		inst._ptr = addr - 1
 		libc.memcpy(addr, libc.dataptr(str), #str)
 	else
-		inst = {
-			_size = 0,
-			_capacity = 32,
-			_ptr = libc.malloc(32) - 1
-		}
+		inst._size = 0
+		inst._capacity = 32
+		inst._ptr = libc.malloc(32) - 1
 	end
-	inst.capacity = function(self)
-		return self._capacity
-	end
-	inst.caddr = function(self)
-		return self._ptr + 1
-	end
-	inst.size = function(self)
-		return self._size
-	end
-	inst.to_string = _to_string
-	inst.reserve = _reserve
-	inst.add = _add
-	inst.dispose = _dispose
-	inst.__gc = _dispose
-	inst.write_to = _write_to
-	inst.get = _get
-	inst.set = _set
-	inst.add_char = _add_char
-	inst.erase = _erase
 	return inst
 end
 function char(str)
