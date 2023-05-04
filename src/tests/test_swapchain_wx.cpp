1c52f9141624fe1272c8a45f6245dea541e9b554
Mike Smith <mango@live.cn>
2023-05-05 02:49:11 +0800

    good

--- src/tests/test_swapchain_wx.cpp
+++ src/tests/test_swapchain_wx.cpp
@@ -24,6 +24,7 @@ private:
     Stream &_stream;
     luisa::unique_ptr<SwapChain> _swapchain;
     luisa::unique_ptr<Image<float>> _image;
+    Framerate _framerate;
 
 public:
     explicit Renderer(wxWindow *parent, Device &device, Stream &stream) noexcept
@@ -46,14 +47,14 @@ public:
         _swapchain = luisa::make_unique<SwapChain>(
             _device.create_swapchain(
                 reinterpret_cast<uint64_t>(GetHandle()),
-                _stream, resolution, false, true, 8));
-
-        Connect(wxEVT_IDLE, wxIdleEventHandler(Renderer::idle));
+                _stream, resolution, false, false, 3));
     }
 
-    void idle(wxIdleEvent &) noexcept {
+    void render() noexcept {
         if (_swapchain == nullptr) { return; }
         _stream << _swapchain->present(*_image);
+        _framerate.record(1u);
+        LUISA_INFO("FPS: {}", _framerate.report());
     }
 };
 
@@ -74,25 +75,6 @@ public:
 
         SetOwnBackgroundColour(color);
         CenterOnParent();
-
-        // draw a line
-        Connect(wxEVT_PAINT, wxPaintEventHandler(Panel::paint));
-        Connect(wxEVT_IDLE, wxIdleEventHandler(Panel::idle));
-    }
-
-    void idle(wxIdleEvent &) noexcept {
-        Refresh();
-    }
-
-    void paint(wxPaintEvent &) noexcept {
-        wxPaintDC dc{this};
-        auto rgb_dist = std::uniform_int_distribution<uint8_t>{0, 255};
-        auto rand_rgb = wxColour{rgb_dist(_rng), rgb_dist(_rng), rgb_dist(_rng)};
-        dc.SetPen(wxPen{rand_rgb});
-        auto size = dc.GetSize();
-        auto x_dist = std::uniform_int_distribution<int>{0, size.x};
-        auto y_dist = std::uniform_int_distribution<int>{0, size.y};
-        dc.DrawLine(x_dist(_rng), y_dist(_rng), x_dist(_rng), y_dist(_rng));
     }
 };
 
@@ -104,6 +86,7 @@ private:
     luisa::unique_ptr<Device> _device;
     luisa::unique_ptr<Stream> _stream;
     Frame *_frame{nullptr};
+    Renderer *_renderer{nullptr};
 
 public:
     bool OnInit() override {
@@ -119,9 +102,9 @@ public:
         _stream = luisa::make_unique<Stream>(_device->create_stream(StreamTag::GRAPHICS));
 
         _frame = new Frame;
-        auto renderer = new Renderer{_frame, *_device, *_stream};
+        _renderer = new Renderer{_frame, *_device, *_stream};
 
-        auto panel = new Panel{_frame, renderer->GetClientSize() / 2, wxColour{128, 64, 96}};
+        auto panel = new Panel{_frame, _renderer->GetClientSize() / 2, wxColour{128, 64, 96}};
 
         auto button = new wxButton{panel, wxID_EXIT, wxT("Quit")};
         Connect(wxID_EXIT, wxEVT_COMMAND_BUTTON_CLICKED,
@@ -130,10 +113,20 @@ public:
         button->SetFocus();
 
         _frame->Show(true);
+
+        Connect(wxID_ANY, wxEVT_IDLE, wxIdleEventHandler(App::idle));
+
         return true;
     }
 
+    void idle(wxIdleEvent &event) noexcept {
+        if (_renderer == nullptr) { return; }
+        _renderer->render();
+        event.RequestMore();
+    }
+
     void close(wxCommandEvent &) noexcept {
+        if (_frame == nullptr) { return; }
         _frame->Close();
     }
 };
