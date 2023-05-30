affc7b4318d75092539db685589f787cf6a994f7
Maxwell Geng <maxwellgeng@outlook.com>
2023-05-30 17:28:43 +0800

    fix ir compile

--- src/ir/xmake.lua
+++ src/ir/xmake.lua
@@ -2,22 +2,27 @@ target("lc-ir")
 _config_project({
 	project_kind = "shared"
 })
-add_defines("LC_IR_EXPORT_DLL")
-add_deps("lc-runtime", "lc-rust")
+on_load(function(target)
+	local function rela(p)
+		return path.relative(path.absolute(p, os.scriptdir()), os.projectdir())
+	end
+	target:add("defines", "LC_IR_EXPORT_DLL")
+	target:add("deps", "lc-runtime", "lc-rust")
+	target:add("includedirs", rela("../rust"), {
+		public = true
+	})
+	if is_plat("windows") then
+		target:add("syslinks", "Ws2_32", "Advapi32", "Bcrypt", "Userenv")
+	end
+	local function add_rs_link(str)
+		target:add("linkdirs", path.absolute(path.join("../rust/target", str), os.scriptdir()))
+		target:add("links", "luisa_compute_api_types", "luisa_compute_ir.dll")
+	end
+	if is_mode("debug") then
+		add_rs_link("debug")
+	else
+		add_rs_link("release")
+	end
+end)
 add_files("**.cpp")
-add_includedirs("../rust", {
-	public = true
-})
-if is_plat("windows") then
-	add_syslinks("Ws2_32", "Advapi32", "Bcrypt", "Userenv")
-end
-function add_rs_link(str)
-	add_links(path.join(path.join("src/rust/target", str), "luisa_compute_api_types"),
-					path.join(path.join("src/rust/target", str), "luisa_compute_ir"))
-end
-if is_mode("debug") then
-	add_rs_link("debug")
-else
-	add_rs_link("release")
-end
 target_end()
