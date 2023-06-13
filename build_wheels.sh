904b9d3c556b58bb5ce408de148c0723ca0140dd
Mike <mango@live.cn>
2023-06-14 02:41:28 +0800

    build for manylinux only

--- build_wheels.sh
+++ build_wheels.sh
@@ -3,5 +3,6 @@
 export CIBW_ARCHS=auto64
 export CIBW_MANYLINUX_X86_64_IMAGE=manylinux_2_28
 export CIBW_BEFORE_ALL="./scripts/cibw_install_deps.sh"
+export CIBW_BUILD_VERBOSITY=2
 
 cibuildwheel --output-dir wheelhouse --platform linux
