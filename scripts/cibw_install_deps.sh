2125dc91af879c40eca702857271e2dfb05a0c4b
Mike <mango@live.cn>
2023-06-13 20:43:55 +0800

    fix permission

--- scripts/cibw_install_deps.sh
+++ scripts/cibw_install_deps.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+uname -a
+
 curl https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.0-1_all.deb -O
-sudo dpkg -i cuda-keyring_1.0-1_all.deb
-sudo apt-get update
-sudo apt-get -y install cuda libvulkan-dev cmake uuid-dev libglfw3-dev libxinerama-dev libxcursor-dev libxi-dev
+dpkg -i cuda-keyring_1.0-1_all.deb
+apt-get update
+apt-get -y install cuda libvulkan-dev cmake uuid-dev libglfw3-dev libxinerama-dev libxcursor-dev libxi-dev
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
