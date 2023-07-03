71769632106802ddb0a90ce416f6b3a103af9b34
Mike Smith <mango@live.cn>
2023-07-04 03:33:18 +0800

    fix cpu struct Hit size

--- src/backends/common/moltenvk_surface.mm
+++ src/backends/common/moltenvk_surface.mm
@@ -9,12 +9,23 @@ namespace luisa::compute {
 
 void *cocoa_window_content_view(uint64_t window_handle) noexcept {
     @autoreleasepool {
-        auto window = (__bridge NSWindow *)(reinterpret_cast<void *>(window_handle));
+        NSView *view = nullptr;
+        auto window_or_view = (__bridge NSObject *)(reinterpret_cast<void *>(window_handle));
+        if ([window_or_view isKindOfClass:[NSWindow class]]) {
+            view = static_cast<NSWindow *>(window_or_view).contentView;
+        } else {
+            if (![window_or_view isKindOfClass:[NSView class]]) {
+                NSLog(@"Invalid window handle %llu of class %@. "
+                       "Expected NSWindow or NSView.",
+                      window_handle, [window_or_view class]);
+            }
+            view = static_cast<NSView *>(window_or_view);
+        }
         auto layer = [CAMetalLayer layer];
-        window.contentView.layer = layer;
-        window.contentView.wantsLayer = YES;
-        window.contentView.layer.contentsScale = window.backingScaleFactor;
-        return window.contentView;
+        view.layer = layer;
+        view.wantsLayer = YES;
+        view.layer.contentsScale = view.window.backingScaleFactor;
+        return view;
     }
 }
 
