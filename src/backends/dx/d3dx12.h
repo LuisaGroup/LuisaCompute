074d06c38b7c970831b54fd61701183de3dd8518
Mike <mango@live.cn>
2023-07-03 21:56:18 +0800

    better d3d12 error message

--- src/backends/dx/d3dx12.h
+++ src/backends/dx/d3dx12.h
@@ -3207,16 +3207,57 @@ inline vstd::wstring AnsiToWString(const vstd::string &str) {
     MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
     return vstd::wstring(buffer);
 }
-#ifndef ThrowIfFailed
-#ifdef NDEBUG
-#define ThrowIfFailed(x) (x)
-#else
-#define ThrowIfFailed(x)     \
-    {                        \
-        HRESULT hr_ = (x);   \
-        assert(hr_ == S_OK); \
+
+inline const char *d3d12_error_name(HRESULT hr) {
+    switch (hr) {
+        case D3D12_ERROR_ADAPTER_NOT_FOUND: return "D3D12_ERROR_ADAPTER_NOT_FOUND";
+        case D3D12_ERROR_DRIVER_VERSION_MISMATCH: return "D3D12_ERROR_DRIVER_VERSION_MISMATCH";
+        case DXGI_ERROR_ACCESS_DENIED: return "DXGI_ERROR_ACCESS_DENIED";
+        case DXGI_ERROR_ACCESS_LOST: return "DXGI_ERROR_ACCESS_LOST";
+        case DXGI_ERROR_ALREADY_EXISTS: return "DXGI_ERROR_ALREADY_EXISTS";
+        case DXGI_ERROR_CANNOT_PROTECT_CONTENT: return "DXGI_ERROR_CANNOT_PROTECT_CONTENT";
+        case DXGI_ERROR_DEVICE_HUNG: return "DXGI_ERROR_DEVICE_HUNG";
+        case DXGI_ERROR_DEVICE_REMOVED: return "DXGI_ERROR_DEVICE_REMOVED";
+        case DXGI_ERROR_DEVICE_RESET: return "DXGI_ERROR_DEVICE_RESET";
+        case DXGI_ERROR_DRIVER_INTERNAL_ERROR: return "DXGI_ERROR_DRIVER_INTERNAL_ERROR";
+        case DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE: return "DXGI_ERROR_GRAPHICS_VIDPN_SOURCE_IN_USE";
+        case DXGI_ERROR_FRAME_STATISTICS_DISJOINT: return "DXGI_ERROR_FRAME_STATISTICS_DISJOINT";
+        case DXGI_ERROR_INVALID_CALL: return "DXGI_ERROR_INVALID_CALL";
+        case DXGI_ERROR_MORE_DATA: return "DXGI_ERROR_MORE_DATA";
+        case DXGI_ERROR_NAME_ALREADY_EXISTS: return "DXGI_ERROR_NAME_ALREADY_EXISTS";
+        case DXGI_ERROR_NONEXCLUSIVE: return "DXGI_ERROR_NONEXCLUSIVE";
+        case DXGI_ERROR_NOT_CURRENTLY_AVAILABLE: return "DXGI_ERROR_NOT_CURRENTLY_AVAILABLE";
+        case DXGI_ERROR_NOT_FOUND: return "DXGI_ERROR_NOT_FOUND";
+        case DXGI_ERROR_REMOTE_CLIENT_DISCONNECTED: return "DXGI_ERROR_REMOTE_CLIENT_DISCONNECTED";
+        case DXGI_ERROR_REMOTE_OUTOFMEMORY: return "DXGI_ERROR_REMOTE_OUTOFMEMORY";
+        case DXGI_ERROR_RESTRICT_TO_OUTPUT_STALE: return "DXGI_ERROR_RESTRICT_TO_OUTPUT_STALE";
+        case DXGI_ERROR_SDK_COMPONENT_MISSING: return "DXGI_ERROR_SDK_COMPONENT_MISSING";
+        case DXGI_ERROR_SESSION_DISCONNECTED: return "DXGI_ERROR_SESSION_DISCONNECTED";
+        case DXGI_ERROR_UNSUPPORTED: return "DXGI_ERROR_UNSUPPORTED";
+        case DXGI_ERROR_WAIT_TIMEOUT: return "DXGI_ERROR_WAIT_TIMEOUT";
+        case DXGI_ERROR_WAS_STILL_DRAWING: return "DXGI_ERROR_WAS_STILL_DRAWING";
+        case E_FAIL: return "E_FAIL";
+        case E_INVALIDARG: return "E_INVALIDARG";
+        case E_OUTOFMEMORY: return "E_OUTOFMEMORY";
+        case E_NOTIMPL: return "E_NOTIMPL";
+        case S_FALSE: return "S_FALSE";
+        case S_OK: return "S_OK";
+        default: break;
     }
-#endif
+    return "Unknown error";
+}
+
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(x)                                       \
+    do {                                                       \
+        HRESULT hr_ = (x);                                     \
+        if (hr_ != S_OK) {                                     \
+            printf("D3D12 call '%s' failed with "              \
+                   "error %s (code = %lld).\n",                \
+                   #x, d3d12_error_name(hr_), (long long)hr_); \
+            abort();                                           \
+        }                                                      \
+    } while (false)
 #endif
 #include <luisa/vstl/unique_ptr.h>
 namespace vstd {
