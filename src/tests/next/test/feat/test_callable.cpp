7b66be71e64ba3075fb7268c19ac48bef7bbb4b9
unknown <colored_black@outlook.com>
2023-07-26 14:59:17 +0800

    fix callable

--- src/tests/next/test/feat/test_callable.cpp
+++ src/tests/next/test/feat/test_callable.cpp
@@ -33,7 +33,7 @@ namespace luisa::test {
 int test_callable(Device &device) {
     log_level_verbose();
     
-    static constexpr uint n = 1024u * 1024u;
+    static constexpr uint n = 10u;
 
     Buffer<float> buffer = device.create_buffer<float>(n);
 
@@ -52,8 +52,7 @@ int test_callable(Device &device) {
     Kernel1D kernel_def = [&](BufferVar<float> source, BufferVar<float> result, Var<float> x) noexcept {
         set_block_size(256u);
         UInt index = dispatch_id().x;
-        auto xx = load(buffer, index);
-        store(result, index, add(load(source, index), x) + xx);
+        store(result, index, add(load(source, index), x));
     };
     Shader1D<Buffer<float>, Buffer<float>, float> kernel = device.compile(kernel_def);
     Stream stream = device.create_stream();
@@ -66,24 +65,28 @@ int test_callable(Device &device) {
     Clock clock;
     stream << buffer.copy_from(data.data());
     CommandList command_list = CommandList::create();
+    command_list << kernel(buffer, result_buffer, 3.0f).dispatch(n);
 
-    for (size_t i = 0; i < 10; i++) {
-        command_list << kernel(buffer, result_buffer, 3).dispatch(n);
-    }
     stream << command_list.commit()
         << result_buffer.copy_to(results.data());
+    
     double t1 = clock.toc();
     stream << synchronize();
     double t2 = clock.toc();
 
     LUISA_INFO("Dispatch in {} ms. Finished in {} ms", t1, t2);
+    LUISA_INFO("Data: {}, {}, {}, {}, ..., {}, {}.",
+               data[0], data[1], data[2], data[3],
+               data[n - 2u], data[n - 1u]);
     LUISA_INFO("Results: {}, {}, {}, {}, ..., {}, {}.",
                results[0], results[1], results[2], results[3],
                results[n - 2u], results[n - 1u]);
 
-    for (size_t i = 0u; i < n; i++) {
-        CHECK_MESSAGE(results[i] == data[i] + 3.0f, "Results mismatch.");
+    for (unsigned int i = 0u; i < n; i++) {
+        // LUISA_INFO("Result: {} || {}", i, results[i] - data[i]);
+        CHECK(results[i] - data[i] == doctest::Approx(3.0f).epsilon(0.001f));
     }
+    return 0;
 }
 
 }// namespace luisa::test
