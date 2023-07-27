6dcbf2d4f135be77bc3635a7371b8e7021504728
unknown <colored_black@outlook.com>
2023-07-27 10:38:12 +0800

    feat: add buffer test suite

--- src/tests/next/test/feat/test_buffer.cpp
+++ src/tests/next/test/feat/test_buffer.cpp
@@ -20,6 +20,114 @@ using namespace luisa::compute;
 
 namespace luisa::test {
 
+template<typename T_FloatX>
+int test_floatx(Device &device, int literal_size = 1, int align_size = 4) {
+    constexpr uint n = 1u;
+    Buffer<T_FloatX> a = device.create_buffer<T_FloatX>(n);
+    Buffer<T_FloatX> b = device.create_buffer<T_FloatX>(n);
+    Buffer<T_FloatX> c = device.create_buffer<T_FloatX>(n);
+
+    Kernel1D add_kernel = [&](BufferVar<T_FloatX> a, BufferVar<T_FloatX> b, BufferVar<T_FloatX> c) noexcept {
+        set_block_size(64u);
+        UInt index = dispatch_id().x;
+        $if(index < n){
+            c->write(index, a->read(index) + b->read(index));
+        };
+    };
+    Shader1D<Buffer<T_FloatX>, Buffer<T_FloatX>, Buffer<T_FloatX>> add = device.compile(add_kernel);
+
+    // init a, b and c
+
+    Stream stream = device.create_stream();
+    luisa::vector<float> data_init(n * align_size, 1.f); 
+    luisa::vector<float> data_result(n * align_size, 0.f);
+    stream << a.copy_from(data_init.data());
+    stream << b.copy_from(data_init.data());
+    stream << c.copy_from(data_result.data());
+
+    stream << add(a, b, c).dispatch(n);
+    stream << synchronize();
+    stream << c.copy_to(data_result.data());
+    stream << synchronize();
+
+    for (uint idx = 0u; idx < n * align_size; idx++) {
+        uint i = idx % align_size;
+        if (align_size != literal_size && i == align_size - 1) {
+            // undefined behavior, depends on backend implementation
+        } else {
+            CHECK_MESSAGE(data_result[idx] == 2.f, "failed when ", idx);
+        }
+    }
+    return 0;
+}
+
+
+int test_float3x3_order(Device &device) {
+    constexpr uint n = 1u;
+    Buffer<float3x3> a = device.create_buffer<float3x3>(n);
+    Buffer<float3x3> b = device.create_buffer<float3x3>(n);
+    Buffer<float3x3> c = device.create_buffer<float3x3>(n);
+
+    Kernel1D add_kernel = [&](BufferVar<float3x3> a, BufferVar<float3x3> b, BufferVar<float3x3> c) noexcept {
+        set_block_size(64u);
+        UInt index = dispatch_id().x;
+        $if(index < n){
+            c->write(index, a->read(index) + b->read(index));
+        };
+    };
+    Shader1D<Buffer<float3x3>, Buffer<float3x3>, Buffer<float3x3>> add = device.compile(add_kernel);
+
+    // init a, b and c
+
+    Stream stream = device.create_stream();
+    luisa::vector<float> data_init(n * 12, 1.f);
+    // align to col major
+    // 1 2 2 
+    // 1 1 2
+    // 1 1 1
+    // 0 0 0
+    // 3 * vec3 : 1 -> 1 -> 1 -> 0 -> 2 -> 1... -> 1 -> 0
+    for (auto i = 0u; i < 3u; i++) {
+        for (auto j = 0u; j < 4u; j++) {
+            if (j == 3) {
+                data_init[i * 4 + j] = 0.f;
+            }
+            else {
+                if (i > j) {
+                    data_init[i * 4 + j] = 2.f;
+                }
+                else {
+                    data_init[i * 4 + j] = 1.f;
+                }
+            }
+        }
+    }
+    luisa::vector<float> data_result(n * 12, 0.f);
+    stream << a.copy_from(data_init.data());
+    stream << b.copy_from(data_init.data());
+    stream << c.copy_from(data_result.data());
+ 
+    stream << add(a, b, c).dispatch(n);
+    stream << synchronize();
+    stream << c.copy_to(data_result.data());
+    stream << synchronize();
+
+    for (uint idx = 0u; idx < n * 12; idx++) {
+        uint i = idx / 4;
+        uint j = idx % 4;
+        if (j == 3) {
+            // undefined behaviour depends on backend implementation
+        } else {
+            if (i > j) {
+                CHECK_MESSAGE(data_result[idx] == 4.f, "failed when ", i, ",", j);
+            } else {
+                CHECK_MESSAGE(data_result[idx] == 2.f, "failed when ", i, ",", j);
+            }
+        }
+    }
+    return 0;
+}
+
 int test_float3x3(Device &device) {
     constexpr uint n = 1u;
     Buffer<float3x3> a = device.create_buffer<float3x3>(n);
@@ -38,8 +146,8 @@ int test_float3x3(Device &device) {
     // init a, b and c
 
     Stream stream = device.create_stream();
-    luisa::vector<float> data_init(n * 9, 1.f);
-    luisa::vector<float> data_result(n * 9, 0.f);
+    luisa::vector<float> data_init(n * 12, 1.f);
+    luisa::vector<float> data_result(n * 12, 0.f);
     stream << a.copy_from(data_init.data());
     stream << b.copy_from(data_init.data());
     stream << c.copy_from(data_result.data());
@@ -49,8 +157,14 @@ int test_float3x3(Device &device) {
     stream << c.copy_to(data_result.data());
     stream << synchronize();
 
-    for (auto idx = 0u; idx < n * 9; idx++) {
-        CHECK_MESSAGE(data_result[idx] == data_init[idx] + data_init[idx], "failed when ", idx);
+    for (uint idx = 0u; idx < n * 12; idx++) {
+        uint i = idx / 4;
+        uint j = idx % 4;
+        if (j == 3) {
+            // undefined behaviour depends on backend implementation
+        } else {
+            CHECK_MESSAGE(data_result[idx] == 2.f, "failed when ", i, ",", j);
+        }
     }
     return 0;
 }
@@ -116,6 +230,18 @@ TEST_SUITE("feat::buffer") {
         }
     }
 
+    TEST_CASE("buffer::float3x3_order") {
+        Context context{luisa::test::argv()[0]};
+       
+        for (auto i = 0; i < luisa::test::supported_backends_count(); i++) {
+            luisa::string device_name = luisa::test::supported_backends()[i];
+            SUBCASE(device_name.c_str()) {
+                Device device = context.create_device(device_name.c_str());
+                REQUIRE(luisa::test::test_float3x3_order(device) == 0);
+            }
+        }
+    }
+
     TEST_CASE("buffer::float4x4") {
         Context context{luisa::test::argv()[0]};
        
@@ -124,6 +250,43 @@ TEST_SUITE("feat::buffer") {
             SUBCASE(device_name.c_str()) {
                 Device device = context.create_device(device_name.c_str());
                 REQUIRE(luisa::test::test_float4x4(device) == 0);
+            }
+        }
+    }
+
+    
+    TEST_CASE("buffer::float4") {
+        Context context{luisa::test::argv()[0]};
+       
+        for (auto i = 0; i < luisa::test::supported_backends_count(); i++) {
+            luisa::string device_name = luisa::test::supported_backends()[i];
+            SUBCASE(device_name.c_str()) {
+                Device device = context.create_device(device_name.c_str());
+                REQUIRE(luisa::test::test_floatx<float4>(device, 4, 4) == 0);
+            }
+        }
+    }
+
+    TEST_CASE("buffer::float3") {
+        Context context{luisa::test::argv()[0]};
+       
+        for (auto i = 0; i < luisa::test::supported_backends_count(); i++) {
+            luisa::string device_name = luisa::test::supported_backends()[i];
+            SUBCASE(device_name.c_str()) {
+                Device device = context.create_device(device_name.c_str());
+                REQUIRE(luisa::test::test_floatx<float4>(device, 3, 4) == 0);
+            }
+        }
+    }
+
+    TEST_CASE("buffer::float2") {
+        Context context{luisa::test::argv()[0]};
+       
+        for (auto i = 0; i < luisa::test::supported_backends_count(); i++) {
+            luisa::string device_name = luisa::test::supported_backends()[i];
+            SUBCASE(device_name.c_str()) {
+                Device device = context.create_device(device_name.c_str());
+                REQUIRE(luisa::test::test_floatx<float2>(device, 2, 2) == 0);
             }
         }
     }
