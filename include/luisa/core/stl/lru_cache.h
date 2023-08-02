9a6d7de109df2b51a0e0ca9a9a3047e0a5da55d5
Mike Smith <mango@live.cn>
2023-08-03 01:11:16 +0800

    DebugCaptureExt for Metal debugging

--- include/luisa/core/stl/lru_cache.h
+++ include/luisa/core/stl/lru_cache.h
@@ -2,13 +2,23 @@
 
 #include <mutex>
 #include <EASTL/bonus/lru_cache.h>
-
+#include <luisa/core/stl/hash_fwd.h>
 #include <luisa/core/stl/memory.h>
 #include <luisa/core/stl/optional.h>
 
 namespace luisa {
 
-using eastl::lru_cache;
+template<typename Key, typename Value, typename Hash = luisa::hash<Key>>
+using lru_cache = eastl::lru_cache<
+    Key, Value,
+    EASTLAllocatorType,
+    eastl::list<Key, EASTLAllocatorType>,
+    eastl::unordered_map<
+        Key,
+        eastl::pair<Value, typename eastl::list<Key, EASTLAllocatorType>::iterator>,
+        luisa::hash<Key>,
+        std::equal_to<Key>,
+        EASTLAllocatorType>>;
 
 // TODO: support allocator & comparator
 template<typename Key, typename Value>
@@ -48,4 +58,3 @@ public:
 };
 
 }// namespace luisa
-
