12112a8cac54061905842177a4280fcfd080ed8c
Mike Smith <mango@live.cn>
2023-10-09 01:30:57 +0800

    fix py ad

--- src/py/export_runtime.cpp
+++ src/py/export_runtime.cpp
@@ -24,7 +24,7 @@ using namespace luisa;
 using namespace luisa::compute;
 constexpr auto pyref = py::return_value_policy::reference;
 using luisa::compute::detail::FunctionBuilder;
-static vstd::vector<ASTEvaluator> analyzer;
+static vstd::vector<luisa::optional<ASTEvaluator>> analyzer;
 struct IntEval {
     int32_t value;
     bool exist;
@@ -502,26 +502,35 @@ void export_runtime(py::module &m) {
             "execute", [](PyStream &self) { self.execute(); }, pyref);
 
     m.def("builder", &FunctionBuilder::current, pyref);
-    m.def("begin_analyzer", []() {
-        analyzer.emplace_back();
+    m.def("begin_analyzer", [](bool enabled) {
+        analyzer.emplace_back(enabled ? luisa::make_optional<ASTEvaluator>() : luisa::nullopt);
     });
     m.def("end_analyzer", []() {
         analyzer.pop_back();
     });
     m.def("begin_branch", [](bool is_loop) {
-        analyzer.back().begin_branch_scope(is_loop);
+        if (auto &&a = analyzer.back()) {
+            a->begin_branch_scope(is_loop);
+        }
     });
     m.def("end_branch", []() {
-        analyzer.back().end_branch_scope();
+        if (auto &&a = analyzer.back()) {
+            a->end_branch_scope();
+        }
     });
     m.def("begin_switch", [](SwitchStmt const *stmt) {
-        analyzer.back().begin_switch(stmt);
+        if (auto &&a = analyzer.back()) {
+            a->begin_switch(stmt);
+        }
     });
     m.def("end_switch", []() {
-        analyzer.back().end_switch();
+        if (auto &&a = analyzer.back()) {
+            a->end_switch();
+        }
     });
     m.def("analyze_condition", [](Expression const *expr) -> int32_t {
-        auto result = analyzer.back().try_eval(expr);
+        ASTEvaluator::Result result;
+        if (auto &&a = analyzer.back()) { result = a->try_eval(expr); }
         return visit(
             [&]<typename T>(T const &t) -> int32_t {
                 if constexpr (std::is_same_v<T, bool>) {
@@ -577,7 +586,8 @@ void export_runtime(py::module &m) {
             return 1;
         })
         .def("try_eval_int", [](FunctionBuilder &self, Expression const *expr) {
-            auto eval = analyzer.back().try_eval(expr);
+            ASTEvaluator::Result eval;
+            if (auto &&a = analyzer.back()) { eval = a->try_eval(expr); }
             return visit(
                 [&]<typename T>(T const &t) -> IntEval {
                     if constexpr (std::is_same_v<T, int32_t> || std::is_same_v<T, uint32_t>) {
@@ -667,11 +677,13 @@ void export_runtime(py::module &m) {
             "call", [](FunctionBuilder &self, const Type *type, CallOp call_op, const luisa::vector<const Expression *> &args) { return self.call(type, call_op, std::move(args)); }, pyref)
         .def(
             "call", [](FunctionBuilder &self, const Type *type, Function custom, const luisa::vector<const Expression *> &args) {
-                analyzer.back().check_call_ref(custom, args);
-                 return self.call(type, custom, std::move(args)); }, pyref)
+                if (auto &&a = analyzer.back()) { a->check_call_ref(custom, args); }
+                return self.call(type, custom, std::move(args));
+            },
+            pyref)
         .def("call", [](FunctionBuilder &self, CallOp call_op, const luisa::vector<const Expression *> &args) { self.call(call_op, std::move(args)); })
         .def("call", [](FunctionBuilder &self, Function custom, const luisa::vector<const Expression *> &args) {
-            analyzer.back().check_call_ref(custom, args);
+            if (auto &&a = analyzer.back()) { a->check_call_ref(custom, args); }
             self.call(custom, std::move(args));
         })
 
@@ -680,7 +692,8 @@ void export_runtime(py::module &m) {
         .def("return_", &FunctionBuilder::return_)
         .def(
             "assign", [](FunctionBuilder &self, Expression const *l, Expression const *r) {
-                auto result = analyzer.back().assign(l, r);
+                ASTEvaluator::Result result;
+                if (auto &&a = analyzer.back()) { result = a->assign(l, r); }
                 visit(
                     [&]<typename T>(T const &t) {
                         if constexpr (std::is_same_v<T, monostate>) {
@@ -704,7 +717,7 @@ void export_runtime(py::module &m) {
         .def(
             "for_", [](FunctionBuilder &self, const Expression *var, const Expression *condition, const Expression *update) {
                 auto ptr = self.for_(var, condition, update);
-                analyzer.back().execute_for(ptr);
+                if (auto &&a = analyzer.back()) { a->execute_for(ptr); }
                 return ptr;
             },
             pyref)
