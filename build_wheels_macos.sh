170ea0cc5a9b93c0168a33f77486c2394d2dd5f0
Mike Smith <mango@live.cn>
2023-11-11 19:18:12 +0800

    minor

--- build_wheels_macos.sh
+++ build_wheels_macos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # build wheels
-#python3.10 -m pip wheel --no-deps --wheel-dir dist/ .
+python3.10 -m pip wheel --no-deps --wheel-dir dist/ .
 python3.11 -m pip wheel --no-deps --wheel-dir dist/ .
-#python3.12 -m pip wheel --no-deps --wheel-dir dist/ .
+python3.12 -m pip wheel --no-deps --wheel-dir dist/ .
 
 # audit wheels
 delocate-wheel dist/*.whl
