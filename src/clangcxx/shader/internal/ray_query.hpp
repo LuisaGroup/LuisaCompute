76aebe1ed2e4ba96af9e75d4e1550156cb51258b
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2023-11-24 21:31:04 +0800

    Update ray_query.hpp

--- src/clangcxx/shader/internal/ray_query.hpp
+++ src/clangcxx/shader/internal/ray_query.hpp
@@ -3,4 +3,18 @@
 #include "type_traits.hpp"
 
 namespace luisa::shader {
-}
+struct RayQueryProceduralProxy {
+    template<typename Func>
+    void on_procedural_candidate(Func &&func);
+};
+struct RayQuerySurfaceProxy {
+    template<typename Func>
+    void on_surface_candidate(Func &&func);
+};
+struct [[builtin("ray_query")]] RayQuery {
+    template<typename Func>
+    RayQueryProceduralProxy on_surface_candidate(Func &&func);
+    template<typename Func>
+    RayQuerySurfaceProxy on_procedural_candidate(Func &&func);
+};
+}// namespace luisa::shader
