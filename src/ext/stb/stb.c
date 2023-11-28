458371377d5a35977feeab07d4155fbc7963b930
Mike <mango@live.cn>
2023-11-28 14:34:32 +0800

    stb

--- src/ext/stb/stb.c
+++ src/ext/stb/stb.c
@@ -1,12 +1,11 @@
-#define STBIR_USE_FMA
-
 #ifdef __aarch64__
 #define STBI_NEON
+#define STBIR_NEON
 #endif
 
 #if _WIN32 || _WIN64
-#define STBIWDEF  _declspec(dllexport)
 #define STBIDEF  _declspec(dllexport)
+#define STBIWDEF  _declspec(dllexport)
 #define STBIRDEF  _declspec(dllexport)
 #endif
 
@@ -16,5 +15,6 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include <stb/stb_image_write.h>
 
+#define STBIR_USE_FMA
 #define STB_IMAGE_RESIZE_IMPLEMENTATION
 #include <stb/stb_image_resize2.h>
