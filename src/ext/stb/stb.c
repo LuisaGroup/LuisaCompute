544affdc94dc4fcee9680d59368a7beae0dcadc6
Mike <mango@live.cn>
2023-11-28 14:29:35 +0800

    enable fma for stb

--- src/ext/stb/stb.c
+++ src/ext/stb/stb.c
@@ -1,11 +1,15 @@
+#define STBIR_USE_FMA
+
 #ifdef __aarch64__
 #define STBI_NEON
 #endif
+
 #if _WIN32 || _WIN64
 #define STBIWDEF  _declspec(dllexport)
 #define STBIDEF  _declspec(dllexport)
 #define STBIRDEF  _declspec(dllexport)
 #endif
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb/stb_image.h>
 
