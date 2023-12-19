d856fe3d6fc33b6f5d98918202a756a1df90e070
SaeruHikari <39457738+SaeruHikari@users.noreply.github.com>
2023-12-19 14:10:59 +0800

    fix typo

--- src/clangcxx/shader/internal/array.hpp
+++ src/clangcxx/shader/internal/array.hpp
@@ -6,29 +6,27 @@ namespace luisa::shader {
 
 template<typename Type, uint64 size, uint32 CacheFlags = 0 /*AUTO*/>
 struct [[builtin("array")]] Array {
-    [[ignore]] Array() = default;
-     
     template <typename...Args>
     [[scope]] Array(Args... args)
     {
         set<0>(args...);
     }
-    
-    template <uint32 start>
-    [[scope]] void set(Type v)
-    {
-        set(start, v);
-    }
+    [[ignore]] Array() = default;
+
     template <uint32 start, typename...Args>
     [[scope]] void set(Type v, Args... args)
     {
         set(start, v);
         set<start + 1>(args...);
     }
-    [[scope]] void set(uint32 loc, Type v) { acess_(loc) = v; }
-
-    [[access]] Type& acess_(uint32 loc);
+    template <uint32 start>
+    [[scope]] void set(Type v) { set(start, v); }
+    [[scope]] void set(uint32 loc, Type v) { access_(loc) = v; }
+    [[access]] Type& access_(uint32 loc);
     [[access]] Type& operator[](uint32 loc);
+    
+    [[scope]] Type get(uint32 loc) const { return access_(loc); }
+    [[access]] Type access_(uint32 loc) const;
     [[access]] Type operator[](uint32 loc) const; 
 private:
     Type v[size];
