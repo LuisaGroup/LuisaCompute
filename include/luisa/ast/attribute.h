f1fefb4c1109c309932b37cbb444ca72d7d53f3e
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-02-03 23:37:07 +0800

    type support attribute

--- include/luisa/ast/attribute.h
+++ include/luisa/ast/attribute.h
@@ -1,19 +1,13 @@
 #pragma once
 #include <luisa/core/stl/string.h>
-#include <luisa/core/stl/unordered_map.h>
-#include <luisa/core/stl/memory.h>
 namespace luisa::compute {
 struct Attribute {
     luisa::string key;
     luisa::string value;
-};
-struct TypeAttribute : public Attribute {
-    luisa::vector<TypeAttribute> elements;
-};
-struct VarAttribute : public Attribute {
-    luisa::shared_ptr<TypeAttribute> type;
-};
-struct FuncAttribute : public Attribute {
-    luisa::shared_ptr<TypeAttribute> type;
+    Attribute() noexcept = default;
+    Attribute(
+        luisa::string &&key,
+        luisa::string &&value) noexcept
+        : key(std::move(key)), value(std::move(value)) {}
 };
 };// namespace luisa::compute
--- include/luisa/ast/function_builder.h
+++ include/luisa/ast/function_builder.h
@@ -100,14 +100,6 @@ private:
         Variable /* argument */,
         const Expression * /* captured */>
         _internalizer_arguments;
-    luisa::unordered_map<
-        uint,
-        VarAttribute>
-        _var_attributes;
-    luisa::unordered_map<
-        Statement *,
-        Attribute>
-        _stmt_attributes;
     luisa::vector<luisa::shared_ptr<const ExternalFunction>> _used_external_functions;
     luisa::vector<luisa::shared_ptr<const FunctionBuilder>> _used_custom_callables;
     luisa::vector<Variable> _local_variables;
@@ -246,19 +238,6 @@ public:
     [[nodiscard]] bool requires_autodiff() const noexcept;
     /// Return if uses printing.
     [[nodiscard]] bool requires_printing() const noexcept;
-
-    [[nodiscard]] auto const &var_attributes() const noexcept {
-        return _var_attributes;
-    }
-    [[nodiscard]] auto const &stmt_attributes() const noexcept {
-        return _stmt_attributes;
-    }
-    [[nodiscard]] auto &var_attributes() noexcept {
-        return _var_attributes;
-    }
-    [[nodiscard]] auto &stmt_attributes() noexcept {
-        return _stmt_attributes;
-    }
 
     // build primitives
     /// Define a kernel function with given definition
