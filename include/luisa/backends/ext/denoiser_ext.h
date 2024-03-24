cb729584681bc334273888d9d6dbd3e57abc48ee
Mike <mango@live.cn>
2024-03-24 16:14:57 +0800

    silence eastl deprecation warnings

--- include/luisa/backends/ext/denoiser_ext.h
+++ include/luisa/backends/ext/denoiser_ext.h
@@ -145,43 +145,43 @@ public:
     };
 };
 
-class [[deprecated]] OldDenoiserExt : public DeviceExtension {
-
-public:
-    struct DenoiserMode {
-        bool kernel_pred = false;//using kernel prediction model, automatically use this for aov model
-        bool temporal = false;   //temporal denoise mode
-        bool alphamode = false;  //alpha channel denoiser mode
-        bool upscale = false;    //upscaling
-        int aov_refract_id = -1; //aov channel index
-        int aov_specular_id = -1;
-        int aov_reflection_id = -1;
-        int aov_diffuse_id = -1;
-    } _mode;
-
-    struct DenoiserInput {
-        const Buffer<float> *beauty = nullptr;
-        const Buffer<float> *normal = nullptr;
-        const Buffer<float> *albedo = nullptr;
-        const Buffer<float> *flow = nullptr;//all 0 for the first frame (if is used)
-        const Buffer<float> *flowtrust = nullptr;
-        Buffer<float> **aovs = nullptr;
-        uint aov_size = 0;
-    };
-
-    static constexpr luisa::string_view name = "OldDenoiserExt";
-
-    //A simple integration for denoising a single image.
-    virtual void denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
-                         Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> **aovs, uint aov_size) noexcept = 0;
-    //initialize the denoiser on device. if using temporal mode, data should be valid when initializing for better performance
-    virtual void init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept = 0;
-    //denoise a certain image.
-    virtual void process(Stream &stream, DenoiserInput input) noexcept = 0;
-    //get the result for one input aov layer, index=-1 for beauty. The output should be created before sent in.
-    virtual void get_result(Stream &stream, Buffer<float> &output, int index = -1) noexcept = 0;
-    //clear the memory usage on device and the internal states held. You should call this before a new initialization.
-    virtual void destroy(Stream &stream) noexcept = 0;
-};
+// class [[deprecated]] OldDenoiserExt : public DeviceExtension {
+//
+// public:
+//     struct DenoiserMode {
+//         bool kernel_pred = false;//using kernel prediction model, automatically use this for aov model
+//         bool temporal = false;   //temporal denoise mode
+//         bool alphamode = false;  //alpha channel denoiser mode
+//         bool upscale = false;    //upscaling
+//         int aov_refract_id = -1; //aov channel index
+//         int aov_specular_id = -1;
+//         int aov_reflection_id = -1;
+//         int aov_diffuse_id = -1;
+//     } _mode;
+//
+//     struct DenoiserInput {
+//         const Buffer<float> *beauty = nullptr;
+//         const Buffer<float> *normal = nullptr;
+//         const Buffer<float> *albedo = nullptr;
+//         const Buffer<float> *flow = nullptr;//all 0 for the first frame (if is used)
+//         const Buffer<float> *flowtrust = nullptr;
+//         Buffer<float> **aovs = nullptr;
+//         uint aov_size = 0;
+//     };
+//
+//     static constexpr luisa::string_view name = "OldDenoiserExt";
+//
+//     //A simple integration for denoising a single image.
+//     virtual void denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
+//                          Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> **aovs, uint aov_size) noexcept = 0;
+//     //initialize the denoiser on device. if using temporal mode, data should be valid when initializing for better performance
+//     virtual void init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept = 0;
+//     //denoise a certain image.
+//     virtual void process(Stream &stream, DenoiserInput input) noexcept = 0;
+//     //get the result for one input aov layer, index=-1 for beauty. The output should be created before sent in.
+//     virtual void get_result(Stream &stream, Buffer<float> &output, int index = -1) noexcept = 0;
+//     //clear the memory usage on device and the internal states held. You should call this before a new initialization.
+//     virtual void destroy(Stream &stream) noexcept = 0;
+// };
 
 }// namespace luisa::compute
--- src/backends/cuda/extensions/cuda_denoiser.h
+++ src/backends/cuda/extensions/cuda_denoiser.h
@@ -29,39 +29,39 @@ public:
 
 namespace luisa::compute::cuda {
 
-class [[deprecated]] CUDAOldDenoiserExt final : public OldDenoiserExt {
-    CUDADevice *_device;
-    std::vector<optix::DenoiserLayer> _layers;
-    optix::Denoiser _denoiser = nullptr;
-    uint32_t _scratch_size = 0;
-    uint32_t _state_size = 0;
-    uint32_t _overlap = 0u;
-    CUdeviceptr _avg_color = 0;
-    CUdeviceptr _scratch = 0;
-    CUdeviceptr _intensity = 0;
-    CUdeviceptr _state = 0;
-    uint2 _resolution;
-    optix::DenoiserGuideLayer _guideLayer = {};
-    void _denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
-                  Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> *aovs, uint aov_size) noexcept;
-    void _init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept;
-    void _process(Stream &stream, DenoiserInput input) noexcept;
-    void _get_result(Stream &stream, Buffer<float> &output, int index) noexcept;
-    void _destroy(Stream &stream) noexcept;
-
-public:
-    CUDAOldDenoiserExt(CUDADevice *device) noexcept : _device(device) {
-    }
-    ~CUDAOldDenoiserExt() noexcept {
-    }
-    void init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept override;
-
-    void process(Stream &stream, DenoiserInput input) noexcept override;
-
-    void get_result(Stream &stream, Buffer<float> &output, int index) noexcept override;
-    void destroy(Stream &stream) noexcept override;
-    void denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
-                 Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> **aovs, uint aov_size) noexcept override;
-};
+// class [[deprecated]] CUDAOldDenoiserExt final : public OldDenoiserExt {
+//     CUDADevice *_device;
+//     std::vector<optix::DenoiserLayer> _layers;
+//     optix::Denoiser _denoiser = nullptr;
+//     uint32_t _scratch_size = 0;
+//     uint32_t _state_size = 0;
+//     uint32_t _overlap = 0u;
+//     CUdeviceptr _avg_color = 0;
+//     CUdeviceptr _scratch = 0;
+//     CUdeviceptr _intensity = 0;
+//     CUdeviceptr _state = 0;
+//     uint2 _resolution;
+//     optix::DenoiserGuideLayer _guideLayer = {};
+//     void _denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
+//                   Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> *aovs, uint aov_size) noexcept;
+//     void _init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept;
+//     void _process(Stream &stream, DenoiserInput input) noexcept;
+//     void _get_result(Stream &stream, Buffer<float> &output, int index) noexcept;
+//     void _destroy(Stream &stream) noexcept;
+//
+// public:
+//     CUDAOldDenoiserExt(CUDADevice *device) noexcept : _device(device) {
+//     }
+//     ~CUDAOldDenoiserExt() noexcept {
+//     }
+//     void init(Stream &stream, DenoiserMode mode, DenoiserInput data, uint2 resolution) noexcept override;
+//
+//     void process(Stream &stream, DenoiserInput input) noexcept override;
+//
+//     void get_result(Stream &stream, Buffer<float> &output, int index) noexcept override;
+//     void destroy(Stream &stream) noexcept override;
+//     void denoise(Stream &stream, uint2 resolution, Buffer<float> const &image, Buffer<float> &output,
+//                  Buffer<float> const &normal, Buffer<float> const &albedo, Buffer<float> **aovs, uint aov_size) noexcept override;
+// };
 
 }// namespace luisa::compute::cuda
