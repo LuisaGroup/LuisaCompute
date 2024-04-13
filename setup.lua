a481f8bac8b76128f77a08a72033aa7aea54f505
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-04-13 13:39:18 +0800

    script for submod

--- setup.lua
+++ setup.lua
@@ -1,5 +1,6 @@
-local packages = import("packages", {rootdir = 'scripts'})
-local find_sdk = import("find_sdk", {rootdir = 'scripts'})
+local rootdir = {rootdir = path.join(os.scriptdir(), 'scripts')}
+local packages = import("packages", rootdir)
+local find_sdk = import("find_sdk", rootdir)
 function main()
     if os.is_host("windows") then
         find_sdk.install_sdk('dx_sdk')
