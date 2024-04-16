ae1ea33e2c4abcf535ac4ac6fd84c4e14bd5c433
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-04-16 10:00:52 +0800

    fix binary

--- setup.lua
+++ setup.lua
@@ -1,12 +1,16 @@
 local rootdir = {rootdir = path.join(os.scriptdir(), 'scripts')}
 local packages = import("packages", rootdir)
 local find_sdk = import("find_sdk", rootdir)
-function main(...)
-    local custom_dir
-    for _,v in ipairs(...) do
-        custom_dir = v
-    end
+function unzip(name, custom_dir)
+    local sdk = packages.sdks()[name]
+    local dir = packages.sdk_dir(os.arch(), custom_dir)
+    find_sdk.unzip_sdk(sdk['name'], dir, dir)
+end
+function main(custom_dir, decompress)
     if os.is_host("windows") then
         find_sdk.install_sdk('dx_sdk', custom_dir)
+    end
+    if decompress then
+        unzip('dx_sdk', custom_dir)  
     end
 end
