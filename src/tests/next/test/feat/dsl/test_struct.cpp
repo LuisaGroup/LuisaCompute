d5c65db54f5361ec440cc88782d0a0ead9896299
sailing-innocent <colored_black@outlook.com>
2024-05-02 17:53:55 +0800

    ub when write back

--- src/tests/next/test/feat/dsl/test_struct.cpp
+++ src/tests/next/test/feat/dsl/test_struct.cpp
@@ -32,14 +32,28 @@ int test_luisa_struct(Device &device) {
     constexpr uint N = 64;
     Buffer<int> int_buf = device.create_buffer<int>(N);
 
+    Dummy dummy{1};
+    CHECK_MESSAGE(dummy.a == 1, "dummy.a should be 1");
     Kernel1D kernel_def = [&](const Var<Dummy> &dummy_var, BufferVar<int> int_buf_var) noexcept {
         set_block_size(32u);
         UInt index = dispatch_id().x;
+        // read
         int_buf_var.write(index, dummy_var.a);
+        $if (index == 0u) {
+            // write
+        };
     };
 
-    Dummy dummy{1};
-    CHECK_MESSAGE(dummy.a == 1, "dummy.a should be 1");
+    Kernel1D catch_kernel_def = [&](BufferVar<int> int_buf_var) noexcept {
+        set_block_size(32u);
+        UInt index = dispatch_id().x;
+        // read
+        int_buf_var.write(index, 2);
+        $if (index == 0u) {
+            // write
+            // dummy.a = 2;
+        };
+    };
 
     auto shader = device.compile(kernel_def);
     stream << shader(dummy, int_buf).dispatch(N);
@@ -47,10 +61,18 @@ int test_luisa_struct(Device &device) {
     std::vector<int> int_result(N);
     stream << int_buf.copy_to(int_result.data());
     stream.synchronize();
-
     for (auto i = 0u; i < N; i++) {
-        CHECK_MESSAGE(int_result[i] == 1, "int_result[i] should be 1");
+        CHECK_MESSAGE(int_result[i] == 1, "int_result[i] after kernel should be 1");
     }
+
+    auto catch_shader = device.compile(catch_kernel_def);
+    stream << catch_shader(int_buf).dispatch(N);
+    stream << int_buf.copy_to(int_result.data());
+    stream.synchronize();
+    for (auto i = 0u; i < N; i++) {
+        CHECK_MESSAGE(int_result[i] == 2, "int_result[i] after catch_kernel should be 2");
+    }
+    // CHECK_MESSAGE(dummy.a == 2, "dummy.a after catch_kernel should be 2");
 
     return 0;
 }
