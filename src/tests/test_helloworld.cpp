1b1e6e08804922a43a0ddab53401ad151d745aea
Maxwell Geng <33229754+MaxwellGengYF@users.noreply.github.com>
2024-05-08 13:54:46 +0800

    update ext

--- src/tests/test_helloworld.cpp
+++ src/tests/test_helloworld.cpp
@@ -1,13 +1,21 @@
-#include <luisa/core/fiber.h>
-#include <luisa/core/logging.h>
-using namespace luisa;
-using namespace luisa::fiber;
+#include <spdlog/fmt/fmt.h>
+#include <string>
+#include <iostream>
+template<typename String, typename Format, typename... Args>
+[[nodiscard]] inline auto format(Format &&f, Args &&...args) noexcept {
+    using char_type = typename String::value_type;
+    using memory_buffer = fmt::basic_memory_buffer<char_type, fmt::inline_buffer_size, std::allocator<char_type>>;
+    memory_buffer buffer;
+    fmt::format_to(std::back_inserter(buffer), std::forward<Format>(f), std::forward<Args>(args)...);
+    return String{buffer.data(), buffer.size()};
+}
+
+template<typename Format, typename... Args>
+[[nodiscard]] inline auto format(Format &&f, Args &&...args) noexcept {
+    return format<std::string>(std::forward<Format>(f), std::forward<Args>(args)...);
+}
 
 int main(int argc, char *argv[]) {
-    scheduler sc;
-    auto ft = async([&](){
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        return 666;
-    });
-    LUISA_INFO("{}", ft.wait());
+    using fmt::format_to;
+    std::cout << fmt::format("shit: {}", argv[0]) << std::endl;
 }
