f0cc77b07160d087b72dbd8fd8e779f408d061ec
Zheng Shaokun <Mango@Live.cn>
2024-07-11 18:39:18 +0800

    fix cuda codegen accel_user_id

--- src/backends/cuda/cuda_codegen_ast.cpp
+++ src/backends/cuda/cuda_codegen_ast.cpp
@@ -976,7 +976,7 @@ void CUDACodegenAST::visit(const CallExpr *expr) {
             break;
         }
         case CallOp::RAY_TRACING_INSTANCE_TRANSFORM: _scratch << "lc_accel_instance_transform"; break;
-        case CallOp::RAY_TRACING_INSTANCE_USER_ID: _scratch << "lc_accel_set_instance_user_id"; break;
+        case CallOp::RAY_TRACING_INSTANCE_USER_ID: _scratch << "lc_accel_instance_user_id"; break;
         // TODO
         case CallOp::RAY_TRACING_INSTANCE_VISIBILITY_MASK: LUISA_NOT_IMPLEMENTED(); break;
         case CallOp::RAY_TRACING_SET_INSTANCE_TRANSFORM: _scratch << "lc_accel_set_instance_transform"; break;
