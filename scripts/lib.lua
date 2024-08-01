998e1f68db0e0c6217e7661061b0800d1e57930e
Yuefeng Geng <maxwellgeng@outlook.com>
2024-08-01 17:03:40 +0800

    optimize string builder

--- scripts/lib.lua
+++ scripts/lib.lua
@@ -16,7 +16,9 @@ function mkdirs(p)
 end
 
 function string_split(str, chr)
-    return str:split(chr, {plain = true})
+    return str:split(chr, {
+        plain = true
+    })
 end
 function string_replace(str, from, to)
     local s, _ = str:gsub(from, to)
@@ -30,7 +32,10 @@ local libc = import("core/base/libc")
 local bytes = import("core/base/bytes")
 local _string_builder = {}
 function _string_builder:to_string()
-    return libc.strndup(self._ptr + 1, self._size)
+    if self._ptr == 0 then
+        return ""
+    end
+    return libc.strndup(self._ptr, self._size)
 end
 local function _add_capacity(self, s)
     local size = s + self._size
@@ -38,14 +43,20 @@ local function _add_capacity(self, s)
     if capa >= size then
         return
     end
-    while capa < size do
-        capa = capa * 2
+    if capa < 32 then
+        capa = 32
+    else
+        while capa < size do
+            capa = capa * 2
+        end
     end
-    local old_ptr = self._ptr + 1
     local new_ptr = libc.malloc(capa)
-    libc.memcpy(new_ptr, old_ptr, self._size)
-    libc.free(old_ptr)
-    self._ptr = new_ptr - 1
+    if self._ptr ~= 0 then
+        local old_ptr = self._ptr
+        libc.memcpy(new_ptr, old_ptr, self._size)
+        libc.free(old_ptr)
+    end
+    self._ptr = new_ptr
     self._capacity = capa
 end
 function _string_builder:reserve(s)
@@ -53,11 +64,13 @@ function _string_builder:reserve(s)
     if capa >= s then
         return
     end
-    local old_ptr = self._ptr + 1
     local new_ptr = libc.malloc(s)
-    libc.memcpy(new_ptr, old_ptr, self._size)
-    libc.free(old_ptr)
-    self._ptr = new_ptr - 1
+    if self._ptr ~= 0 then
+        local old_ptr = self._ptr
+        libc.memcpy(new_ptr, old_ptr, self._size)
+        libc.free(old_ptr)
+    end
+    self._ptr = new_ptr
     self._capacity = s
 end
 function _string_builder:equal(str)
@@ -73,7 +86,10 @@ function _string_builder:equal(str)
     if str_size ~= self.size() then
         return false
     end
-    local ptr = self._ptr + self._size + 1
+    if str_size == 0 then
+        return true
+    end
+    local ptr = self._ptr + self._size
     return libc.memcmp(ptr, str_ptr, str_size) == 0
 end
 function _string_builder:add(str)
@@ -87,10 +103,10 @@ function _string_builder:add(str)
         str_size = str:size()
     end
     if str_size == 0 then
-        return
+        return self
     end
     _add_capacity(self, str_size)
-    local ptr = self._ptr + self._size + 1
+    local ptr = self._ptr + self._size
     libc.memcpy(ptr, str_ptr, str_size)
     self._size = self._size + str_size
     return self
@@ -98,7 +114,7 @@ end
 function _string_builder:subview(offset, size)
     local sf = self
     return {
-        _size = math.min(sf._size - offset + 1, size),
+        _size = math.min(sf._size - offset, size),
         _ptr = sf._ptr + offset,
         size = function(self)
             return self._size
@@ -110,13 +126,13 @@ function _string_builder:subview(offset, size)
 end
 function _string_builder:add_char(c)
     _add_capacity(self, 1)
-    self._size = self._size + 1
     libc.setbyte(self._ptr, self._size, c)
+    self._size = self._size + 1
     return self
 end
 function _string_builder:dispose()
     if self._ptr ~= -1 then
-        libc.free(self._ptr + 1)
+        libc.free(self._ptr)
         self._ptr = -1
     end
 end
@@ -126,13 +142,13 @@ function _string_builder:write_to(path)
     f:close()
 end
 function _string_builder:get(i)
-    return libc.byteof(self._ptr, i)
+    return libc.byteof(self._ptr, i - 1)
 end
 function _string_builder:set(i, v)
-    return libc.setbyte(self._ptr, i, v)
+    return libc.setbyte(self._ptr, i - 1, v)
 end
 function _string_builder:erase(i)
-    self._size = math.max(self._size - i, 1)
+    self._size = math.max(self._size - i, 0)
 end
 function _string_builder:size()
     return self._size
@@ -141,10 +157,10 @@ function _string_builder:capacity()
     return self._capacity
 end
 function _string_builder:caddr()
-    return self._ptr + 1
+    return self._ptr
 end
 function _string_builder:cdata()
-    return self._ptr + 1
+    return self._ptr
 end
 function _string_builder:clear()
     self._size = 0
@@ -154,23 +170,31 @@ function StringBuilder(str)
     if str then
         local str_ptr
         local str_size
+        local function set_string()
+            local capa = math.max(32, str_size)
+            local addr = libc.malloc(capa)
+            inst._size = str_size
+            inst._capacity = capa
+            inst._ptr = addr
+            libc.memcpy(addr, str_ptr, str_size)
+        end
         if type(str) == "string" then
             str_ptr = libc.dataptr(str)
             str_size = #str
+            set_string()
+        elseif type(str) == "number" then
+            inst._size = 0
+            inst._capacity = str
+            inst._ptr = libc.malloc(str)
         else
             str_ptr = str:caddr()
             str_size = str:size()
+            set_string()
         end
-        local capa = math.max(32, str_size)
-        local addr = libc.malloc(capa)
-        inst._size = str_size
-        inst._capacity = capa
-        inst._ptr = addr - 1
-        libc.memcpy(addr, str_ptr, str_size)
     else
         inst._size = 0
-        inst._capacity = 32
-        inst._ptr = libc.malloc(32) - 1
+        inst._capacity = 0
+        inst._ptr = 0
     end
     return inst
 end
