e6b158a5f5f58ec1fb84d335158df7de2daf0bf1
Mike Smith <mango@live.cn>
2024-09-22 12:36:37 +0800

    [wip] cuda motion instance: transform assembly

--- src/backends/cuda/cuda_motion_instance.cpp
+++ src/backends/cuda/cuda_motion_instance.cpp
@@ -13,12 +13,6 @@ CUDAMotionInstance::CUDAMotionInstance(CUDADevice *device, const AccelMotionOpti
     : CUDAPrimitiveBase{CUDAPrimitiveBase::Tag::MOTION_INSTANCE}, _option{option} {
     auto [traversable_type, buffer_size] = [&] {
         switch (option.mode) {
-            case AccelOption::MotionMode::STATIC: {
-                LUISA_ASSERT(option.keyframe_count == 1u,
-                             "Static motion instance should have only one keyframe.");
-                auto size = sizeof(optix::StaticTransform);
-                return std::make_pair(optix::TRAVERSABLE_TYPE_STATIC_TRANSFORM, size);
-            }
             case AccelOption::MotionMode::MATRIX: {
                 LUISA_ASSERT(option.keyframe_count >= 2u,
                              "Matrix motion instance should have at least two keyframes.");
@@ -52,8 +46,81 @@ CUDAMotionInstance::~CUDAMotionInstance() noexcept {
 
 void CUDAMotionInstance::build(CUDACommandEncoder &encoder,
                                MotionInstanceBuildCommand *command) noexcept {
+    // note that we do not need to lock the mutex here,
+    // since none of the members change during build
     LUISA_ASSERT(command->keyframes().size() == _option.keyframe_count,
                  "Keyframe count mismatch.");
+    // allocate host-pinned memory for keyframes
+    auto child = reinterpret_cast<CUDAPrimitive *>(command->child())->handle();
+    encoder.with_upload_buffer(
+        _motion_buffer_size,
+        [&encoder, option = _option, child,
+         buffer = _motion_buffer, size = _motion_buffer_size,
+         keys = command->steal_keyframes()](auto view) noexcept {
+            struct MotionTransform {
+                optix::TraversableHandle child;
+                optix::MotionOptions motionOptions;
+                unsigned int pad[3];
+                [[no_unique_address]] float data[];
+            };
+            auto p = reinterpret_cast<MotionTransform *>(view->address());
+            {
+                p->child = child;
+                p->motionOptions.numKeys = option.keyframe_count;
+                p->motionOptions.flags = optix::MOTION_FLAG_NONE;
+                if (option.should_vanish_start) { p->motionOptions.flags |= optix::MOTION_FLAG_START_VANISH; }
+                if (option.should_vanish_end) { p->motionOptions.flags |= optix::MOTION_FLAG_END_VANISH; }
+                p->motionOptions.timeBegin = option.time_start;
+                p->motionOptions.timeEnd = option.time_end;
+            }
+            switch (option.mode) {
+                case AccelOption::MotionMode::MATRIX: {
+                    auto m = reinterpret_cast<std::array<float, 12> *>(p->data);
+                    // m is a 3x4 row-major matrix, while the input is 4x4 column-major
+                    for (auto i = 0u; i < option.keyframe_count; i++) {
+                        m[i][0] = keys[i].as_matrix()[0][0];
+                        m[i][1] = keys[i].as_matrix()[1][0];
+                        m[i][2] = keys[i].as_matrix()[2][0];
+                        m[i][3] = keys[i].as_matrix()[3][0];
+                        m[i][4] = keys[i].as_matrix()[0][1];
+                        m[i][5] = keys[i].as_matrix()[1][1];
+                        m[i][6] = keys[i].as_matrix()[2][1];
+                        m[i][7] = keys[i].as_matrix()[3][1];
+                        m[i][8] = keys[i].as_matrix()[0][2];
+                        m[i][9] = keys[i].as_matrix()[1][2];
+                        m[i][10] = keys[i].as_matrix()[2][2];
+                        m[i][11] = keys[i].as_matrix()[3][2];
+                    }
+                    break;
+                }
+                case AccelOption::MotionMode::SRT: {
+                    auto m = reinterpret_cast<optix::SRTData *>(p->data);
+                    for (auto i = 0u; i < option.keyframe_count; i++) {
+                        m[i].pvx = keys[i].as_srt().pivot[0];
+                        m[i].pvy = keys[i].as_srt().pivot[1];
+                        m[i].pvz = keys[i].as_srt().pivot[2];
+                        m[i].qx = keys[i].as_srt().quaternion[0];
+                        m[i].qy = keys[i].as_srt().quaternion[1];
+                        m[i].qz = keys[i].as_srt().quaternion[2];
+                        m[i].qw = keys[i].as_srt().quaternion[3];
+                        m[i].sx = keys[i].as_srt().scale[0];
+                        m[i].sy = keys[i].as_srt().scale[1];
+                        m[i].sz = keys[i].as_srt().scale[2];
+                        m[i].a = keys[i].as_srt().shear[0];
+                        m[i].b = keys[i].as_srt().shear[1];
+                        m[i].c = keys[i].as_srt().shear[2];
+                        m[i].tx = keys[i].as_srt().translation[0];
+                        m[i].ty = keys[i].as_srt().translation[1];
+                        m[i].tz = keys[i].as_srt().translation[2];
+                    }
+                    break;
+                }
+                default:
+                    LUISA_ERROR_WITH_LOCATION("Unsupported motion mode.");
+            }
+            LUISA_CHECK_CUDA(cuMemcpyHtoDAsync(
+                buffer, view->address(), size, encoder.stream()->handle()));
+        });
 }
 
 }// namespace luisa::compute::cuda
