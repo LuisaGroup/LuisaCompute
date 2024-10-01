e7550905c78d50b5f968949ee81c12a6b48c6957
Mike Smith <mango@live.cn>
2024-10-01 23:38:01 +0800

    work around metal compiler bug

--- src/backends/metal/metal_builtin_embedded.h
+++ src/backends/metal/metal_builtin_embedded.h
@@ -1,4 +1,4 @@
 #pragma once
 
 extern "C" const char luisa_metal_builtin_metal_builtin_kernels[7214];
-extern "C" const char luisa_metal_builtin_metal_device_lib[65381];
+extern "C" const char luisa_metal_builtin_metal_device_lib[65390];
