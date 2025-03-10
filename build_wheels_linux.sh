339d12ef46849196bea4fba694481a2b4901f1aa
Johnny <johnnync13@gmail.com>
2025-03-10 11:29:04 +0100

    Update build_wheels_linux.sh

--- build_wheels_linux.sh
+++ build_wheels_linux.sh
@@ -3,6 +3,7 @@
 export CIBW_ARCHS=auto64
 export CIBW_BUILD="*manylinux*"
 export CIBW_MANYLINUX_X86_64_IMAGE=manylinux_2_28
+export CIBW_MANYLINUX_AARCH_64_IMAGE=manylinux_2_28
 export CIBW_BEFORE_ALL="./scripts/cibw_install_deps.sh"
 export CIBW_BUILD_VERBOSITY=2
 export CIBW_REPAIR_WHEEL_COMMAND="auditwheel show {wheel} && auditwheel repair -w {dest_dir} {wheel} --exclude libcuda.so.1 --exclude libvulkan.so.1"
