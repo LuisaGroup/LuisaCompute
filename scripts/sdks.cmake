eee720a9260b7bb86d90d78da343c3975079856c
MaxwellGeng <maxwellgeng@outlook.com>
2026-06-26 07:56:46 +0800

    Update sdks.cmake

--- scripts/sdks.cmake
+++ scripts/sdks.cmake
@@ -1,3 +1,3 @@
 # SDK metadata for LuisaCompute
 # Each SDK is stored as a CMake list: "download_url" "sha256"
-set(LUISA_COMPUTE_DX_SDK "https://github.com/LuisaGroup/SDKs/releases/download/sdk/dx_sdk_20260622.zip" "fe59aa342087116c9f946828d4463e64b88700da513a7a5a10e230595bbb38cd")
+set(LUISA_COMPUTE_DX_SDK "https://github.com/LuisaGroup/SDKs/releases/download/sdk/dx_sdk_20260622.zip" "c351c6a60443959c0b301834d553fb48759d2eeb3d6c33685b3f06c092a3a970")
