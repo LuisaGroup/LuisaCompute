a317b9e1fff987fb1fe4e8646da2a492f229f7f6
Mike Smith <mango@live.cn>
2026-08-21 11:04:26 +0800

    metal: order host callbacks before event completion

--- src/backends/metal/metal_event.h
+++ src/backends/metal/metal_event.h
@@ -1,15 +1,21 @@
 #pragma once
 
+#include <atomic>
+
+#include <luisa/core/stl/memory.h>
 #include <luisa/core/stl/string.h>
 #include <luisa/core/spin_mutex.h>
 #include "metal_api.h"
 
 namespace luisa::compute::metal {
 
+struct MetalCallbackContext;
+
 class MetalEvent {
 
 private:
     MTL::SharedEvent *_handle;
+    luisa::shared_ptr<std::atomic_uint64_t> _host_completed_value;
 
 public:
     explicit MetalEvent(MTL::Device *device) noexcept;
@@ -17,10 +23,11 @@ public:
     [[nodiscard]] auto handle() const noexcept { return _handle; }
     [[nodiscard]] bool is_completed(uint64_t value) const noexcept;
     void signal(MTL::CommandBuffer *command_buffer, uint64_t value) noexcept;
+    [[nodiscard]] MetalCallbackContext *host_signal_callback(
+        uint64_t value) const noexcept;
     void wait(MTL::CommandBuffer *command_buffer, uint64_t value) noexcept;
     void synchronize(uint64_t value) noexcept;
     void set_name(luisa::string_view name) noexcept;
 };
 
 }// namespace luisa::compute::metal
-
